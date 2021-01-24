a43cf2846c7606a5f05dc508df3e4e49db0cce9b
Luke Wren <wren6991@gmail.com>
2021-01-24 16:42:14 +0000

    Add flash_get_unique_id to hardware_flash

--- src/rp2_common/hardware_flash/include/hardware/flash.h
+++ src/rp2_common/hardware_flash/include/hardware/flash.h
@@ -18,6 +18,8 @@
 #define FLASH_SECTOR_SIZE (1u << 12)
 #define FLASH_BLOCK_SIZE (1u << 16)
 
+#define FLASH_UNIQUE_ID_SIZE_BYTES 8
+
 /** \file flash.h
  *  \defgroup hardware_flash hardware_flash
  *
@@ -27,6 +29,10 @@
  * executing from flash. In this case you must perform your own
  * synchronisation to make sure no XIP accesses take place during flash
  * programming.
+ *
+ * Likewise they are *unsafe* if you have interrupt handlers or an interrupt
+ * vector table in flash, so you must disable interrupts before calling in
+ * this case.
  *
  * If PICO_NO_FLASH=1 is not defined (i.e. if the program is built to run from
  * flash) then these functions will make a static copy of the second stage
@@ -54,6 +60,19 @@ void flash_range_erase(uint32_t flash_offs, size_t count);
  * \param data Pointer to the data to program into flash
  * \param count Number of bytes to program. Must be a multiple of 256 bytes (one page).
  */
+
 void flash_range_program(uint32_t flash_offs, const uint8_t *data, size_t count);
+
+/*! \brief Get flash unique 64 bit identifier
+ *  \ingroup hardware_flash
+ *
+ * Use a standard 4Bh RUID instruction to retrieve the 64 bit unique
+ * identifier from a flash device attached to the QSPI interface. Since there
+ * is a 1:1 association between the MCU and this flash, this also serves as a
+ * unique identifier for the board.
+ *
+ *  \param id_out Pointer to an 8-byte buffer to which the ID will be written
+ */
+void flash_get_unique_id(uint8_t *id_out);
 
 #endif
