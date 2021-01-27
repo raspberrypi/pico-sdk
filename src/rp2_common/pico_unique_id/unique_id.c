bade47df680867f5205a2e1f88b82cb167cad707
Luke Wren <wren6991@gmail.com>
2021-01-27 14:04:14 +0000

    Add pico_unique_board_id_t struct and rename some functions/defines

--- src/rp2_common/pico_unique_id/unique_id.c
+++ src/rp2_common/pico_unique_id/unique_id.c
@@ -7,23 +7,22 @@
 #include "hardware/flash.h"
 #include "pico/unique_id.h"
 
-static_assert(PICO_UNIQUE_ID_SIZE_BYTES == FLASH_UNIQUE_ID_SIZE_BYTES);
+static_assert(PICO_UNIQUE_BOARD_ID_SIZE_BYTES == FLASH_UNIQUE_ID_SIZE_BYTES);
 
-static uint8_t retrieved_id[PICO_UNIQUE_ID_SIZE_BYTES];
+static pico_unique_board_id_t retrieved_id;
 
 static void __attribute__((constructor)) _retrieve_unique_id_on_boot() {
 #if PICO_NO_FLASH
     // The hardware_flash call will panic() if called directly on a NO_FLASH
     // build. Since this constructor is pre-main it would be annoying to
     // debug, so just produce something well-defined and obviously wrong.
-    for (int i = 0; i < PICO_UNIQUE_ID_SIZE_BYTES; i++)
-        retrieved_id[i] = 0xee;
+    for (int i = 0; i < PICO_UNIQUE_BOARD_ID_SIZE_BYTES; i++)
+        retrieved_id.id[i] = 0xee;
 #else
-    flash_get_unique_id(retrieved_id);
+    flash_get_unique_id(retrieved_id.id);
 #endif
 }
 
-void pico_get_unique_id(uint8_t *id_out) {
-    for (int i = 0; i < PICO_UNIQUE_ID_SIZE_BYTES; i++)
-        id_out[i] = retrieved_id[i];
+void pico_get_unique_board_id(pico_unique_board_id_t *id_out) {
+    *id_out = retrieved_id;
 }
