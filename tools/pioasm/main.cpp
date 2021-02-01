1f11051458a143d8d7f6f67c58e8a9faf44c2412
graham sanderson <graham.sanderson@raspberrypi.com>
2021-02-01 10:04:34 -0600

    remove static order dependency

--- tools/pioasm/main.cpp
+++ tools/pioasm/main.cpp
@@ -18,7 +18,7 @@ void usage() {
     std::cerr << "\n";
     std::cerr << "options:\n";
     std::cerr << "  -o <output_format>   select output_format (default '" << DEFAULT_OUTPUT_FORMAT << "'); available options are:\n";
-    for(const auto& f : output_format::output_formats) {
+    for(const auto& f : output_format::all()) {
         std::cerr << "                           " << f->name << std::endl;
         std::cerr << "                               " << f->get_description() << std::endl;
     }
@@ -79,11 +79,11 @@ int main(int argc, char *argv[]) {
     }
     std::shared_ptr<output_format> oformat;
     if (!res) {
-        const auto& e = std::find_if(output_format::output_formats.begin(), output_format::output_formats.end(),
+        const auto& e = std::find_if(output_format::all().begin(), output_format::all().end(),
                                      [&](const std::shared_ptr<output_format> &f) {
                                          return f->name == format;
                                      });
-        if (e == output_format::output_formats.end()) {
+        if (e == output_format::all().end()) {
             std::cerr << "error: unknown output format '" << format << "'" << std::endl;
             res = 1;
         } else {
