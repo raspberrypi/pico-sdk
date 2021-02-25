f239bd3de583f863988bf2d4ce5e327f72db13cb
Andrew Scheller <andrew.scheller@raspberrypi.com>
2021-02-25 17:13:42 +0000

    Fixup PICO_FLASH_SIZE_BYTES PICO_CONFIG entry (no default)

--- src/rp2_common/hardware_flash/include/hardware/flash.h
+++ src/rp2_common/hardware_flash/include/hardware/flash.h
@@ -44,7 +44,7 @@
 
 #define FLASH_UNIQUE_ID_SIZE_BYTES 8
 
-// PICO_CONFIG: PICO_FLASH_SIZE_BYTES, size of primary flash in bytes, type=int, default=-1, group=hardware_flash
+// PICO_CONFIG: PICO_FLASH_SIZE_BYTES, size of primary flash in bytes, type=int, group=hardware_flash
 #ifndef PICO_FLASH_SIZE_BYTES
 #warning PICO_FLASH_SIZE_BYTES is not set
 #endif
