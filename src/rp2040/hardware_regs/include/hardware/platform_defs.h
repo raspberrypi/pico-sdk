6770bd2a4694fc0e207b2c0ec0210368595ed4fe
graham sanderson <graham.sanderson@raspberrypi.com>
2021-03-03 09:39:54 -0600

    use _u(0x800) insteand of 0x800u in platform_defs which is included by assembler

--- src/rp2040/hardware_regs/include/hardware/platform_defs.h
+++ src/rp2040/hardware_regs/include/hardware/platform_defs.h
@@ -9,41 +9,41 @@
 
 // This header is included from C and assembler - only define macros
 
-#define NUM_CORES 2u
-#define NUM_DMA_CHANNELS 12u
-#define NUM_IRQS 32u
-#define NUM_PIOS 2u
-#define NUM_PIO_STATE_MACHINES 4u
-#define NUM_PWM_SLICES 8u
-#define NUM_SPIN_LOCKS 32u
-#define NUM_UARTS 2u
-#define NUM_BANK0_GPIOS 30u
-
-#define PIO_INSTRUCTION_COUNT 32u
-
-#define XOSC_MHZ 12u
-
-// PICO_CONFIG: PICO_STACK_SIZE, Stack Size, min=0x100, default=0x800, advanced=true, group=pico_standard_link
-#ifndef PICO_STACK_SIZE
-#define PICO_STACK_SIZE 0x800u
-#endif
-
-// PICO_CONFIG: PICO_HEAP_SIZE, Heap size to reserve, min=0x100, default=0x800, advanced=true, group=pico_standard_link
-#ifndef PICO_HEAP_SIZE
-#define PICO_HEAP_SIZE 0x800
-#endif
-
-// PICO_CONFIG: PICO_NO_RAM_VECTOR_TABLE, Enable/disable the RAM vector table, type=bool, default=0, advanced=true, group=pico_runtime
-#ifndef PICO_NO_RAM_VECTOR_TABLE
-#define PICO_NO_RAM_VECTOR_TABLE 0
-#endif
-
 #ifndef _u
 #ifdef __ASSEMBLER__
 #define _u(x) x
 #else
 #define _u(x) x ## u
 #endif
+#endif
+
+#define NUM_CORES _u(2)
+#define NUM_DMA_CHANNELS _u(12)
+#define NUM_IRQS _u(32)
+#define NUM_PIOS _u(2)
+#define NUM_PIO_STATE_MACHINES _u(4)
+#define NUM_PWM_SLICES _u(8)
+#define NUM_SPIN_LOCKS _u(32)
+#define NUM_UARTS _u(2)
+#define NUM_BANK0_GPIOS _u(30)
+
+#define PIO_INSTRUCTION_COUNT _u(32)
+
+#define XOSC_MHZ _u(12)
+
+// PICO_CONFIG: PICO_STACK_SIZE, Stack Size, min=0x100, default=0x800, advanced=true, group=pico_standard_link
+#ifndef PICO_STACK_SIZE
+#define PICO_STACK_SIZE _u(0x800)
+#endif
+
+// PICO_CONFIG: PICO_HEAP_SIZE, Heap size to reserve, min=0x100, default=0x800, advanced=true, group=pico_standard_link
+#ifndef PICO_HEAP_SIZE
+#define PICO_HEAP_SIZE _u(0x800)
+#endif
+
+// PICO_CONFIG: PICO_NO_RAM_VECTOR_TABLE, Enable/disable the RAM vector table, type=bool, default=0, advanced=true, group=pico_runtime
+#ifndef PICO_NO_RAM_VECTOR_TABLE
+#define PICO_NO_RAM_VECTOR_TABLE 0
 #endif
 
 #endif
