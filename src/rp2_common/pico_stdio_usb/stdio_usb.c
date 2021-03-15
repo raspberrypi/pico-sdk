e7693b23bbfca2a471fc81e528b7379a39f25455
graham sanderson <graham.sanderson@raspberrypi.com>
2021-03-15 14:45:01 -0500

    pico_stdio_usb: be more explicit about includes, fix warning (#257)

--- src/rp2_common/pico_stdio_usb/stdio_usb.c
+++ src/rp2_common/pico_stdio_usb/stdio_usb.c
@@ -10,12 +10,13 @@
 #include "pico/time.h"
 #include "pico/stdio/driver.h"
 #include "pico/binary_info.h"
+#include "pico/mutex.h"
 #include "hardware/irq.h"
 
 static_assert(PICO_STDIO_USB_LOW_PRIORITY_IRQ > RTC_IRQ, ""); // note RTC_IRQ is currently the last one
 static mutex_t stdio_usb_mutex;
 
-static void low_priority_worker_irq() {
+static void low_priority_worker_irq(void) {
     // if the mutex is already owned, then we are in user code
     // in this file which will do a tud_task itself, so we'll just do nothing
     // until the next tick; we won't starve
