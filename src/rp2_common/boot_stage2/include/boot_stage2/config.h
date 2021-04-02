f4332d112f48d1254a6fae199d7e8acb4772b620
Andrew Scheller <github@loowis.durge.org>
2021-04-02 11:55:43 +0100

    Tweaks to PICO_BOOT_STAGE2_CHOOSE_* so that tools/extract_configs.py works again (also fix some typos)

--- src/rp2_common/boot_stage2/include/boot_stage2/config.h
+++ src/rp2_common/boot_stage2/include/boot_stage2/config.h
@@ -9,25 +9,36 @@
 
 // NOTE THIS HEADER IS INCLUDED FROM ASSEMBLY
 
+#include "pico/config.h"
+
 // PICO_CONFIG: PICO_BUILD_BOOT_STAGE2_NAME, The name of the boot stage 2 if selected by the build, group=boot_stage2
-// PICO_CONFIG: PICO_BOOT_STAGE2_CHOOSE_IS25LP080, Select boot2_is25lp080 as the boot stage 2 when no boot stage2 selection is made by the CMake build, type=bool, default=false, group=boot_stage2
-// PICO_CONFIG: PICO_BOOT_STAGE2_CHOOSE_W25Q080, Select boot2_w28q080 as the boot stage 2 when no boot stage2 selection is made by the CMake build, type=bool, default=false, group=boot_stage2
-// PICO_CONFIG: PICO_BOOT_STAGE2_CHOOSE_W25X10CL, Select boot2_is25lp080 as the boot stage 2 when no boot stage2 selection is made by the CMake build, type=bool, default=false, group=boot_stage2
-// PICO_CONFIG: PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H, Select boot2_generic_03h as the boot stage 2 when no boot stage2 selection is made by the CMake build, type=bool, default=true, group=boot_stage2
+// PICO_CONFIG: PICO_BOOT_STAGE2_CHOOSE_IS25LP080, Select boot2_is25lp080 as the boot stage 2 when no boot stage2 selection is made by the CMake build, type=bool, default=0, group=boot_stage2
+#ifndef PICO_BOOT_STAGE2_CHOOSE_IS25LP080
+    #define PICO_BOOT_STAGE2_CHOOSE_IS25LP080 0
+#endif
+// PICO_CONFIG: PICO_BOOT_STAGE2_CHOOSE_W25Q080, Select boot2_w25q080 as the boot stage 2 when no boot stage2 selection is made by the CMake build, type=bool, default=0, group=boot_stage2
+#ifndef PICO_BOOT_STAGE2_CHOOSE_W25Q080
+    #define PICO_BOOT_STAGE2_CHOOSE_W25Q080 0
+#endif
+// PICO_CONFIG: PICO_BOOT_STAGE2_CHOOSE_W25X10CL, Select boot2_w25x10cl as the boot stage 2 when no boot stage2 selection is made by the CMake build, type=bool, default=0, group=boot_stage2
+#ifndef PICO_BOOT_STAGE2_CHOOSE_W25X10CL
+    #define PICO_BOOT_STAGE2_CHOOSE_W25X10CL 0
+#endif
+// PICO_CONFIG: PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H, Select boot2_generic_03h as the boot stage 2 when no boot stage2 selection is made by the CMake build, type=bool, default=1, group=boot_stage2
 
 #ifdef PICO_BUILD_BOOT_STAGE2_NAME
     // boot stage2 is configured by cmake, so use the name specified there
     #define PICO_BOOT_STAGE2_NAME PICO_BUILD_BOOT_STAGE2_NAME
 #else
-    // boot stage2 is selected by board configu header, so we have to do some work
+    // boot stage2 is selected by board config header, so we have to do some work
     // NOTE: this switch is mirrored in compile_time_choice.S
     #if PICO_BOOT_STAGE2_CHOOSE_IS25LP080
         #define PICO_BOOT_STAGE2_NAME "boot2_is25lp080"
     #elif PICO_BOOT_STAGE2_CHOOSE_W25Q080
-        #define PICO_BOOT_STAGE2_NAME "boot2_w28q080"
+        #define PICO_BOOT_STAGE2_NAME "boot2_w25q080"
     #elif PICO_BOOT_STAGE2_CHOOSE_W25X10CL
         #define PICO_BOOT_STAGE2_NAME "boot2_w25x10cl"
-    #elif PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H || !defined(PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H)
+    #elif !defined(PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H) || PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H
         #undef PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H
         #define PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H 1
         #define PICO_BOOT_STAGE2_NAME "boot2_generic_03h"
