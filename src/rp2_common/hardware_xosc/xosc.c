9056b8eecc7ac6c63d1bded27c3647e1e10a9bfd
Dan Halbert <halbert@halwitz.org>
2021-05-28 12:52:00 -0400

    Define PICO_XOSC_STARTUP_DELAY_MULTIPLIER as a PICO_CONFIG

--- src/rp2_common/hardware_xosc/xosc.c
+++ src/rp2_common/hardware_xosc/xosc.c
@@ -13,7 +13,7 @@
 #include "hardware/regs/xosc.h"
 #include "hardware/structs/xosc.h"
 
-// Allow lengthening startup delay to accommodate slow-starting oscillators
+// PICO_CONFIG: PICO_XOSC_STARTUP_DELAY_MULTIPLIER, Multiplier to lengthen xosc startup delay to accommodate slow-starting oscillators, type=int, min=1, default=1, group=hardware_xosc
 #ifndef PICO_XOSC_STARTUP_DELAY_MULTIPLIER
 #define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 1
 #endif
