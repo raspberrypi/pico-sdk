73536682dbf8d5169b253a7c031b8f1953668fa9
leoli51 <leo_la_rocca@yahoo.it>
2021-09-17 16:08:35 +0200

    minor fixes

--- src/boards/include/boards/melopero_shake.h
+++ src/boards/include/boards/melopero_shake.h
@@ -9,16 +9,11 @@
 //       SHOULD ONLY CONSIST OF PREPROCESSOR DIRECTIVES
 // -----------------------------------------------------
 
-#ifndef _BOARDS_MELOPERO_SHAKE_H
-#define _BOARDS_MELOPERO_SHAKE_H
+#ifndef _BOARDS_MELOPERO_SHAKE_RP2040_H
+#define _BOARDS_MELOPERO_SHAKE_RP2040_H
 
 // For board detection
-#define MELOPERO_SHAKE
-
-// On some samples, the xosc can take longer to stabilize than is usual
-#ifndef PICO_XOSC_STARTUP_DELAY_MULTIPLIER
-#define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 64
-#endif
+#define MELOPERO_SHAKE_RP2040
 
 //------------- UART -------------//
 #ifndef PICO_DEFAULT_UART
@@ -44,7 +39,7 @@
 
 //------------- I2C -------------//
 #ifndef PICO_DEFAULT_I2C
-#define PICO_DEFAULT_I2C 0
+#define PICO_DEFAULT_I2C 1
 #endif
 
 #ifndef PICO_DEFAULT_I2C_SDA_PIN
@@ -72,7 +67,12 @@
 #define PICO_DEFAULT_SPI_SCK_PIN 18
 #endif
 
+#ifndef PICO_DEFAULT_SPI_CSN_PIN
+#define PICO_DEFAULT_SPI_CSN_PIN 1
+#endif
+
 //------------- FLASH -------------//
+
 
 #define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
 
@@ -81,18 +81,15 @@
 #endif
 
 #ifndef PICO_FLASH_SIZE_BYTES
-#define PICO_FLASH_SIZE_BYTES (2 * 1024 * 1024)
+#define PICO_FLASH_SIZE_BYTES (16 * 1024 * 1024)
 #endif
 
-// Drive high to force power supply into PWM mode (lower ripple on 3V3 at light loads)
-#define PICO_SMPS_MODE_PIN 23
-
 #ifndef PICO_FLOAT_SUPPORT_ROM_V1
-#define PICO_FLOAT_SUPPORT_ROM_V1 1
+#define PICO_FLOAT_SUPPORT_ROM_V1 0
 #endif
 
 #ifndef PICO_DOUBLE_SUPPORT_ROM_V1
-#define PICO_DOUBLE_SUPPORT_ROM_V1 1
+#define PICO_DOUBLE_SUPPORT_ROM_V1 0
 #endif
 
 #endif
