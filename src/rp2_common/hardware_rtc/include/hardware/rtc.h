8b64e9b4502542a8f867690b703e96665d07c39a
graham sanderson <graham.sanderson@raspberrypi.com>
2022-05-17 12:39:28 -0500

    remove comma

--- src/rp2_common/hardware_rtc/include/hardware/rtc.h
+++ src/rp2_common/hardware_rtc/include/hardware/rtc.h
@@ -48,7 +48,7 @@ void rtc_init(void);
  *  \ingroup hardware_rtc
  *
  * \note Note that after setting the RTC date and time, a subsequent read of the values (e.g. via rtc_get_datetime()) may not
- * reflect the new setting, until up to three cycles of the potentially-much-slower RTC clock domain have passed. This represents a period
+ * reflect the new setting until up to three cycles of the potentially-much-slower RTC clock domain have passed. This represents a period
  * of 64 microseconds with the default RTC clock configuration.
  *
  * \param t Pointer to a \ref datetime_t structure contains time to set
