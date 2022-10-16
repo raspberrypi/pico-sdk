a3be48356d32859443820b9249c333e7e96590d4
Graham Sanderson <graham.sanderson@raspberrypi.com>
2022-10-16 16:57:54 -0500

    tweak fix

--- src/boards/generic_board.cmake
+++ src/boards/generic_board.cmake
@@ -21,5 +21,3 @@ else()
     string(CONCAT msg ${msg} "   Looked for ${PICO_BOARD}.cmake in ${DIRS} (additional paths specified by PICO_BOARD_CMAKE_DIRS)")
     message(FATAL_ERROR ${msg})
 endif()
-
-set(PICO_CYW43_SUPPORTED "0" CACHE INTERNAL "Exclude support for PICO_CYW43")
