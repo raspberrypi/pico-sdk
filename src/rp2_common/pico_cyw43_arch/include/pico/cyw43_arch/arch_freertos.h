4e1cbf900febf9060caec0f4a108e21fee6d62c7
Peter Harper <peter.harper@raspberrypi.com>
2023-01-23 18:04:40 +0000

    Add config for cyw43 freertos task stack and priority
    CYW43_TASK_STACK_SIZE and CYW43_TASK_PRIORITY

--- src/rp2_common/pico_cyw43_arch/include/pico/cyw43_arch/arch_freertos.h
+++ src/rp2_common/pico_cyw43_arch/include/pico/cyw43_arch/arch_freertos.h
@@ -13,6 +13,16 @@
 extern "C" {
 #endif
 
+// PICO_CONFIG: CYW43_TASK_STACK_SIZE, Stack size for the cyw43_task thread in 4byte words, type=int, default=1024, group=pico_cyw43_arch
+#ifndef CYW43_TASK_STACK_SIZE
+#define CYW43_TASK_STACK_SIZE 1024
+#endif
+
+// PICO_CONFIG: CYW43_TASK_PRIORITY, Priority for the cyw43_task thread, type=int default=4, group=pico_cyw43_arch
+#ifndef CYW43_TASK_PRIORITY
+#define CYW43_TASK_PRIORITY (tskIDLE_PRIORITY + 4)
+#endif
+
 #ifdef __cplusplus
 }
 #endif
