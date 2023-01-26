fefb6b6d1e08d5991ab095846ad88b5cf0611c72
Graham Sanderson <graham.sanderson@raspberrypi.com>
2023-01-26 16:05:29 -0600

    Move more code from pico_cyw43_arch to pico_cyw43_driver (#1201)
    Basically the integration code (cyw43_config.h and related implementations) are now in the driver.
    cyw43_arch now just has
      * async_context creation per CYW43_ARCH_TYPE
      * pre-existing cyw43_arch methods for connect etc.

--- src/rp2_common/pico_cyw43_arch/include/pico/cyw43_arch/arch_threadsafe_background.h
+++ src/rp2_common/pico_cyw43_arch/include/pico/cyw43_arch/arch_threadsafe_background.h
@@ -7,14 +7,6 @@
 #ifndef _PICO_CYW43_ARCH_ARCH_THREADSAFE_BACKGROUND_H
 #define _PICO_CYW43_ARCH_ARCH_THREADSAFE_BACKGROUND_H
 
-#include "pico/cyw43_arch/arch_common.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#ifdef __cplusplus
-}
-#endif
+// now obsolete; kept for backwards compatibility
 
 #endif
