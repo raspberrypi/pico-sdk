bd2a71bd2c1d37b1b83a9cdcd51131a48f4c0882
lurk101 <lurkone0one@gmail.com>
2023-02-14 02:11:10 -0500

    Run pre-installed pioasm and elf2uf2 if found in system search path

--- tools/FindPioasm.cmake
+++ tools/FindPioasm.cmake
@@ -10,44 +10,56 @@
 #
 
 if (NOT Pioasm_FOUND)
-    # todo we would like to use pckgconfig to look for it first
-    # see https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right/
-
-    include(ExternalProject)
-
-    set(PIOASM_SOURCE_DIR ${PICO_SDK_PATH}/tools/pioasm)
-    set(PIOASM_BINARY_DIR ${CMAKE_BINARY_DIR}/pioasm)
-
-    set(PioasmBuild_TARGET PioasmBuild)
-    set(Pioasm_TARGET Pioasm)
-
-    if (NOT TARGET ${PioasmBuild_TARGET})
-        pico_message_debug("PIOASM will need to be built")
-#        message("Adding external project ${PioasmBuild_Target} in ${CMAKE_CURRENT_LIST_DIR}}")
-        ExternalProject_Add(${PioasmBuild_TARGET}
-                PREFIX pioasm
-                SOURCE_DIR ${PIOASM_SOURCE_DIR}
-                BINARY_DIR ${PIOASM_BINARY_DIR}
-                CMAKE_ARGS "-DCMAKE_MAKE_PROGRAM:FILEPATH=${CMAKE_MAKE_PROGRAM}"
-                CMAKE_CACHE_ARGS "-DPIOASM_EXTRA_SOURCE_FILES:STRING=${PIOASM_EXTRA_SOURCE_FILES}"
-                BUILD_ALWAYS 1 # force dependency checking
-                INSTALL_COMMAND ""
-                )
-    endif()
-
-    if (CMAKE_HOST_WIN32)
-        set(Pioasm_EXECUTABLE ${PIOASM_BINARY_DIR}/pioasm.exe)
+    find_program(Pioasm_EXECUTABLE pioasm)
+    if (NOT "${Pioasm_EXECUTABLE}" STREQUAL "Pioasm_EXECUTABLE-NOTFOUND")
+       set(Pioasm_TARGET Pioasm)
+       if(NOT TARGET ${Pioasm_TARGET})
+           add_executable(${Pioasm_TARGET} IMPORTED)
+       endif()
+       set_property(TARGET ${Pioasm_TARGET} PROPERTY IMPORTED_LOCATION
+               ${Pioasm_EXECUTABLE})
+       message("Using ${Pioasm_EXECUTABLE}")
     else()
-        set(Pioasm_EXECUTABLE ${PIOASM_BINARY_DIR}/pioasm)
-    endif()
-    if(NOT TARGET ${Pioasm_TARGET})
-#        message("Adding executable ${Pioasm_Target} in ${CMAKE_CURRENT_LIST_DIR}")
-        add_executable(${Pioasm_TARGET} IMPORTED)
-    endif()
-    set_property(TARGET ${Pioasm_TARGET} PROPERTY IMPORTED_LOCATION
-            ${Pioasm_EXECUTABLE})
+       # todo we would like to use pckgconfig to look for it first
+       # see https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right/
 
-#    message("EXE is ${Pioasm_EXECUTABLE}")
-    add_dependencies(${Pioasm_TARGET} ${PioasmBuild_TARGET})
+       include(ExternalProject)
+
+       set(PIOASM_SOURCE_DIR ${PICO_SDK_PATH}/tools/pioasm)
+       set(PIOASM_BINARY_DIR ${CMAKE_BINARY_DIR}/pioasm)
+
+       set(PioasmBuild_TARGET PioasmBuild)
+       set(Pioasm_TARGET Pioasm)
+
+       if (NOT TARGET ${PioasmBuild_TARGET})
+           pico_message_debug("PIOASM will need to be built")
+#           message("Adding external project ${PioasmBuild_Target} in ${CMAKE_CURRENT_LIST_DIR}}")
+           ExternalProject_Add(${PioasmBuild_TARGET}
+                   PREFIX pioasm 
+                   SOURCE_DIR ${PIOASM_SOURCE_DIR}
+                   BINARY_DIR ${PIOASM_BINARY_DIR}
+                   CMAKE_ARGS "-DCMAKE_MAKE_PROGRAM:FILEPATH=${CMAKE_MAKE_PROGRAM}"
+                   CMAKE_CACHE_ARGS "-DPIOASM_EXTRA_SOURCE_FILES:STRING=${PIOASM_EXTRA_SOURCE_FILES}"
+                   BUILD_ALWAYS 1 # force dependency checking
+                   INSTALL_COMMAND ""
+                   )
+       endif()
+
+       if (CMAKE_HOST_WIN32)
+           set(Pioasm_EXECUTABLE ${PIOASM_BINARY_DIR}/pioasm.exe)
+       else()
+           set(Pioasm_EXECUTABLE ${PIOASM_BINARY_DIR}/pioasm)
+       endif()
+       if(NOT TARGET ${Pioasm_TARGET})
+#           message("Adding executable ${Pioasm_Target} in ${CMAKE_CURRENT_LIST_DIR}")
+           add_executable(${Pioasm_TARGET} IMPORTED)
+       endif()
+       set_property(TARGET ${Pioasm_TARGET} PROPERTY IMPORTED_LOCATION
+               ${Pioasm_EXECUTABLE})
+
+#       message("EXE is ${Pioasm_EXECUTABLE}")
+       add_dependencies(${Pioasm_TARGET} ${PioasmBuild_TARGET})
+    endif()
     set(Pioasm_FOUND 1)
 endif()
+
