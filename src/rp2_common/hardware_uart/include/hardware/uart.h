0b0632a47bf003516a784764660ef9970f0b27b2
Peter Harper <peter.harper@raspberrypi.com>
2024-07-23 15:43:51 +0100

    Fix doxygen for uart example
    Fixes https://github.com/raspberrypi/pico-sdk/issues/1685

--- src/rp2_common/hardware_uart/include/hardware/uart.h
+++ src/rp2_common/hardware_uart/include/hardware/uart.h
@@ -56,12 +56,13 @@ extern "C" {
  *  \code
  *  int main() {
  *
- *     // Initialise UART 0
- *     uart_init(uart0, 115200);
- *
  *     // Set the GPIO pin mux to the UART - 0 is TX, 1 is RX
+ *     // Do this before calling uart_init to avoid losing data
  *     gpio_set_function(0, GPIO_FUNC_UART);
  *     gpio_set_function(1, GPIO_FUNC_UART);
+ *
+ *     // Initialise UART 0
+ *     uart_init(uart0, 115200);
  *
  *     uart_puts(uart0, "Hello world!");
  * }
