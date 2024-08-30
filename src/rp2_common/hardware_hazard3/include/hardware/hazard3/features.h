917b12d32e9112f9e93a730c73144c215a7d3dba
Graham Sanderson <graham.sanderson@raspberrypi.com>
2024-08-30 11:06:59 -0500

    make header guard consistent

--- src/rp2_common/hardware_hazard3/include/hardware/hazard3/features.h
+++ src/rp2_common/hardware_hazard3/include/hardware/hazard3/features.h
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#ifndef _HARDWARE_HAZARD3_FEATURES_
-#define _HARDWARE_HAZARD3_FEATURES_
+#ifndef _HARDWARE_HAZARD3_FEATURES_H
+#define _HARDWARE_HAZARD3_FEATURES_H
 
 #include "pico.h"
 
