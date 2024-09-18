780817a8c744dc0ae2ca10b1f81e103fb4fbfcff
David Goffredo <dmgoffredo@gmail.com>
2024-09-18 15:17:18 -0400

    Fix comments next to `enum gpio_irq_level` values. (#1928)
    The comments for the values of `enum gpio_irq_level` mix up zero and
    one. This commit replaces "0" with "1" and vice versa where appropriate
    in those comments.

--- src/rp2_common/hardware_gpio/include/hardware/gpio.h
+++ src/rp2_common/hardware_gpio/include/hardware/gpio.h
@@ -188,10 +188,10 @@ enum gpio_dir {
  * cleared by writing to the INTR register.
  */
 enum gpio_irq_level {
-    GPIO_IRQ_LEVEL_LOW = 0x1u,  ///< IRQ when the GPIO pin is a logical 1
-    GPIO_IRQ_LEVEL_HIGH = 0x2u, ///< IRQ when the GPIO pin is a logical 0
-    GPIO_IRQ_EDGE_FALL = 0x4u,  ///< IRQ when the GPIO has transitioned from a logical 0 to a logical 1
-    GPIO_IRQ_EDGE_RISE = 0x8u,  ///< IRQ when the GPIO has transitioned from a logical 1 to a logical 0
+    GPIO_IRQ_LEVEL_LOW = 0x1u,  ///< IRQ when the GPIO pin is a logical 0
+    GPIO_IRQ_LEVEL_HIGH = 0x2u, ///< IRQ when the GPIO pin is a logical 1
+    GPIO_IRQ_EDGE_FALL = 0x4u,  ///< IRQ when the GPIO has transitioned from a logical 1 to a logical 0
+    GPIO_IRQ_EDGE_RISE = 0x8u,  ///< IRQ when the GPIO has transitioned from a logical 0 to a logical 1
 };
 
 /*! Callback function type for GPIO events
