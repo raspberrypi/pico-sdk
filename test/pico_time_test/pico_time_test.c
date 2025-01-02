bec3f5b634cf03132791b7202a546863404ffb4e
Peter Harper <peter.harper@raspberrypi.com>
2025-01-02 19:59:36 +0000

    Add a delay after rtc_set_datetime
    If you call rtc_get_datetime immediately after rtc_set_datetime you get
    junk back. According to the datasheet "Writing to the RTC will take
    2 clk_rtc clock periods to arrive". So add a delay after calling
    rtc_set_datetime in aon_timer_set_time_calendar.
    
    Fixes #2148

--- test/pico_time_test/pico_time_test.c
+++ test/pico_time_test/pico_time_test.c
@@ -11,6 +11,7 @@
 #include "hardware/clocks.h"
 #include "pico/stdlib.h"
 #include "pico/test.h"
+#include "pico/aon_timer.h"
 // Include sys/types.h before inttypes.h to work around issue with
 // certain versions of GCC and newlib which causes omission of PRIi64
 #include <sys/types.h>
@@ -72,10 +73,11 @@ static bool repeating_timer_callback(struct repeating_timer *t) {
 #define RESOLUTION_ALLOWANCE PICO_HARDWARE_TIMER_RESOLUTION_US
 #endif
 
-int issue_195_test(void);
-int issue_1812_test(void);
-int issue_1953_test(void);
-int issue_2118_test(void);
+static int issue_195_test(void);
+static int issue_1812_test(void);
+static int issue_1953_test(void);
+static int issue_2118_test(void);
+static int issue_2148_test(void);
 
 int main() {
     setup_default_uart();
@@ -250,6 +252,8 @@ int main() {
 
     issue_2118_test();
 
+    issue_2148_test();
+
     PICOTEST_END_TEST();
 }
 
@@ -260,7 +264,7 @@ int64_t issue_195_callback(alarm_id_t id, void *user_data) {
     return -ISSUE_195_TIMER_DELAY;
 }
 
-int issue_195_test(void) {
+static int issue_195_test(void) {
     PICOTEST_START_SECTION("Issue #195 race condition - without fix may hang on gcc 10.2.1 release builds");
     absolute_time_t t1 = get_absolute_time();
     int id = add_alarm_in_us(ISSUE_195_TIMER_DELAY, issue_195_callback, NULL, true);
@@ -279,7 +283,7 @@ int issue_195_test(void) {
 }
 
 // Setting an alarm should not swallow a sev
-int issue_1812_test(void) {
+static int issue_1812_test(void) {
     PICOTEST_START_SECTION("Issue #1812 defect - Setting an alarm should not ignore a sev");
 
     __sev(); // Make sure the call below does not ignore this
@@ -303,7 +307,7 @@ static void alarm_pool_stuck_issue_1953(uint alarm) {
     hard_assert(false);
 }
 
-int issue_1953_test(void) {
+static int issue_1953_test(void) {
     PICOTEST_START_SECTION("Issue #1953 defect - Alarm can be set in the past");
     int alarm = hardware_alarm_claim_unused(true);
     hardware_alarm_set_callback(alarm, alarm_pool_stuck_issue_1953);
@@ -336,7 +340,7 @@ static bool timer_callback_issue_2118(repeating_timer_t *rt) {
     return true;
 }
 
-int issue_2118_test(void) {
+static int issue_2118_test(void) {
     PICOTEST_START_SECTION("Issue #2118 defect - failure to set an alarm");
 
     // this problem only happens when running the clock fast as it requires the time between
@@ -363,4 +367,34 @@ int issue_2118_test(void) {
 
     PICOTEST_END_SECTION();
     return 0;
+}
+
+static int issue_2148_test(void) {
+#if HAS_RP2040_RTC
+    PICOTEST_START_SECTION("Issue #2148 defect - get time after rtc start");
+    struct tm tm = { 0 };
+    struct tm tm_check = { 0 };
+
+    tm.tm_sec = 55;
+    tm.tm_min = 36;
+    tm.tm_hour = 20;
+    tm.tm_mday = 21;
+    tm.tm_mon = 10;
+    tm.tm_year = 124;
+    tm.tm_wday = 4;
+    tm.tm_yday = 325;
+    tm.tm_isdst = 0;
+    hard_assert(aon_timer_start_calendar(&tm));
+    hard_assert(aon_timer_get_time_calendar(&tm_check));
+
+    PICOTEST_CHECK(tm.tm_sec == tm_check.tm_sec || tm.tm_sec == tm_check.tm_sec - 1, "failed to get seconds");
+    PICOTEST_CHECK(tm.tm_min == tm_check.tm_min, "failed to get minutes");
+    PICOTEST_CHECK(tm.tm_hour == tm_check.tm_hour, "failed to get hour");
+    PICOTEST_CHECK(tm.tm_mday == tm_check.tm_mday, "failed to get day");
+    PICOTEST_CHECK(tm.tm_mon == tm_check.tm_mon, "failed to get month");
+    PICOTEST_CHECK(tm.tm_year == tm_check.tm_year, "failed to get year");
+
+    aon_timer_stop();
+    PICOTEST_END_SECTION();
+#endif
 }
