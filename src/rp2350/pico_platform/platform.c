339a3aaf957405987ecabd2b942bb2d834d76bba
graham sanderson <graham.sanderson@raspberrypi.com>
2025-03-26 16:18:03 -0500

    collapse 2 asserts to 1

--- src/rp2350/pico_platform/platform.c
+++ src/rp2350/pico_platform/platform.c
@@ -16,9 +16,9 @@ uint8_t rp2350_chip_version(void) {
     uint32_t chip_id = *((io_ro_32*)(SYSINFO_BASE + SYSINFO_CHIP_ID_OFFSET));
     uint32_t __unused manufacturer = chip_id & SYSINFO_CHIP_ID_MANUFACTURER_BITS;
     uint32_t __unused part = (chip_id & SYSINFO_CHIP_ID_PART_BITS) >> SYSINFO_CHIP_ID_PART_LSB;
-    assert(manufacturer == MANUFACTURER_RPI);
-    assert(part == PART_RP4);
+    assert(manufacturer == MANUFACTURER_RPI && part == PART_RP4);
     // 0 == A0, 1 == A1, 2 == A2
-    uint version = (chip_id & SYSINFO_CHIP_ID_REVISION_BITS) >> SYSINFO_CHIP_ID_REVISION_LSB;
+    uint32_t version = (chip_id & SYSINFO_CHIP_ID_REVISION_BITS) >> SYSINFO_CHIP_ID_REVISION_LSB;
+    version = (version & 3u) | ((version & 8u) >> 1);
     return (uint8_t)version;
 }
