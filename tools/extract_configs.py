raspberrypi/pico-sdk

commit d639292fea8646f3598c26f2c6734a1b17818e0a

Update extract configs (#1845)
* Update config-extraction scripts to work with multiple chips

* Standardise wording between common options

* Various config-related fixes

* Update pico_rand to use correct busctrl_hw struct-name

* Don't start config descriptions with "The ..."

tools/extract_configs.py
@@ -7,11 +7,11 @@
 #
 # Script to scan the Raspberry Pi Pico SDK tree searching for configuration items
 # Outputs a tab separated file of the configuration item:
-# name	location	description	type	advanced	default	depends	enumvalues	group	max	min
+# name	location	platform	chip	description	type	advanced	default	depends	enumvalues	group	max	min
 #
 # Usage:
 #
-# tools/extract_configs.py <root of source tree> [output file]
+# tools/extract_configs.py <root of repo> [output file]
 #
 # If not specified, output file will be `pico_configs.tsv`
 
@@ -22,19 +22,30 @@ import re
 import csv
 import logging
 
+from collections import defaultdict
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 scandir = sys.argv[1]
 outfile = sys.argv[2] if len(sys.argv) > 2 else 'pico_configs.tsv'
 
-CONFIG_RE = re.compile(r'//\s+PICO_CONFIG:\s+(\w+),\s+([^,]+)(?:,\s+(.*))?$')
+BASE_CONFIG_NAME = 'PICO_CONFIG'
+BASE_CONFIG_RE = re.compile(r'\b{}\b'.format(BASE_CONFIG_NAME))
+BASE_CMAKE_CONFIG_NAME = 'PICO_CMAKE_CONFIG'
+BASE_CMAKE_CONFIG_RE = re.compile(r'\b{}\b'.format(BASE_CMAKE_CONFIG_NAME))
+BASE_BUILD_DEFINE_NAME = 'PICO_BUILD_DEFINE'
+BASE_BUILD_DEFINE_RE = re.compile(r'\b{}\b'.format(BASE_BUILD_DEFINE_NAME))
+
+CONFIG_RE = re.compile(r'//\s+{}:\s+(\w+),\s+([^,]+)(?:,\s+(.*))?$'.format(BASE_CONFIG_NAME))
 DEFINE_RE = re.compile(r'#define\s+(\w+)\s+(.+?)(\s*///.*)?$')
 
-all_configs = {}
+CHIP_NAMES = ["rp2040", "rp2350"]
+
+chips_all_configs = defaultdict(dict)
 all_attrs = set()
-all_descriptions = {}
-all_defines = {}
+chips_all_descriptions = defaultdict(dict)
+chips_all_defines = defaultdict(dict)
 
 
 
@@ -105,7 +116,7 @@ def ValidateAttrs(config_attrs, file_path, linenum):
             if _default not in _enumvalues:
                 raise Exception('{} at {}:{} has default value {} which isn\'t in list of enumvalues {}'.format(config_name, file_path, linenum, config_attrs['default'], config_attrs['enumvalues']))
     else:
-        raise Exception("Found unknown PICO_CONFIG type {} at {}:{}".format(_type, file_path, linenum))
+        raise Exception("Found unknown {} type {} at {}:{}".format(BASE_CONFIG_NAME, _type, file_path, linenum))
 
 
 
@@ -117,55 +128,68 @@ for dirpath, dirnames, filenames in os.walk(scandir):
         file_ext = os.path.splitext(filename)[1]
         if file_ext in ('.c', '.h'):
             file_path = os.path.join(dirpath, filename)
+            applicable = "all"
+            for chip in (*CHIP_NAMES, "host"):
+                if "/{}/".format(chip) in dirpath:
+                    applicable = chip
+                    break
 
             with open(file_path, encoding="ISO-8859-1") as fh:
                 linenum = 0
                 for line in fh.readlines():
                     linenum += 1
                     line = line.strip()
-                    m = CONFIG_RE.match(line)
-                    if m:
-                        config_name = m.group(1)
-                        config_description = m.group(2)
-                        _attrs = m.group(3)
-                        # allow commas to appear inside brackets by converting them to and from NULL chars
-                        _attrs = re.sub(r'(\(.+\))', lambda m: m.group(1).replace(',', '\0'), _attrs)
+                    if BASE_CMAKE_CONFIG_RE.search(line):
+                        raise Exception("Found {} at {}:{} ({}) which isn't expected in {} files".format(BASE_CMAKE_CONFIG_NAME, file_path, linenum, line, file_ext))
+                    elif BASE_BUILD_DEFINE_RE.search(line):
+                        raise Exception("Found {} at {}:{} ({}) which isn't expected in {} files".format(BASE_BUILD_DEFINE_NAME, file_path, linenum, line, file_ext))
+                    elif BASE_CONFIG_RE.search(line):
+                        m = CONFIG_RE.match(line)
+                        if not m:
+                            if line.startswith("//// "):
+                                logger.info("Possible misformatted {} at {}:{} ({})".format(BASE_CONFIG_NAME, file_path, linenum, line))
+                            else:
+                                raise Exception("Found misformatted {} at {}:{} ({})".format(BASE_CONFIG_NAME, file_path, linenum, line))
+                        else:
+                            config_name = m.group(1)
+                            config_description = m.group(2)
+                            _attrs = m.group(3)
+                            # allow commas to appear inside brackets by converting them to and from NULL chars
+                            _attrs = re.sub(r'(\(.+\))', lambda m: m.group(1).replace(',', '\0'), _attrs)
 
-                        if '=' in config_description:
-                            raise Exception("For {} at {}:{} the description was set to '{}' - has the description field been omitted?".format(config_name, file_path, linenum, config_description))
-                        if config_description in all_descriptions:
-                            # relax check for the same header/variable in a different tree
-                            if config_name != all_descriptions[config_description]['config_name'] or filename != all_descriptions[config_description]['filename_only']:
+                            if '=' in config_description:
+                                raise Exception("For {} at {}:{} the description was set to '{}' - has the description field been omitted?".format(config_name, file_path, linenum, config_description))
+                            all_descriptions = chips_all_descriptions[applicable]
+                            if config_description in all_descriptions:
                                 raise Exception("Found description {} at {}:{} but it was already used at {}:{}".format(config_description, file_path, linenum, os.path.join(scandir, all_descriptions[config_description]['filename']), all_descriptions[config_description]['line_number']))
-                        else:
-                            all_descriptions[config_description] = {'config_name': config_name, 'filename': os.path.relpath(file_path, scandir), 'filename_only':filename, 'line_number': linenum}
+                            else:
+                                all_descriptions[config_description] = {'config_name': config_name, 'filename': os.path.relpath(file_path, scandir), 'line_number': linenum}
 
-                        config_attrs = {}
-                        prev = None
-                        # Handle case where attr value contains a comma
-                        for item in _attrs.split(','):
-                            if "=" not in item:
-                                assert(prev)
-                                item = prev + "," + item
-                            try:
-                                k, v = (i.strip() for i in item.split('='))
-                            except ValueError:
-                                raise Exception('{} at {}:{} has malformed value {}'.format(config_name, file_path, linenum, item))
-                            config_attrs[k] = v.replace('\0', ',')
-                            all_attrs.add(k)
-                            prev = item
-                        #print(file_path, config_name, config_attrs)
+                            config_attrs = {}
+                            prev = None
+                            # Handle case where attr value contains a comma
+                            for item in _attrs.split(','):
+                                if "=" not in item:
+                                    assert(prev)
+                                    item = prev + "," + item
+                                try:
+                                    k, v = (i.strip() for i in item.split('='))
+                                except ValueError:
+                                    raise Exception('{} at {}:{} has malformed value {}'.format(config_name, file_path, linenum, item))
+                                config_attrs[k] = v.replace('\0', ',')
+                                all_attrs.add(k)
+                                prev = item
+                            #print(file_path, config_name, config_attrs)
 
-                        if 'group' not in config_attrs:
-                            raise Exception('{} at {}:{} has no group attribute'.format(config_name, file_path, linenum))
+                            if 'group' not in config_attrs:
+                                raise Exception('{} at {}:{} has no group attribute'.format(config_name, file_path, linenum))
 
-                        #print(file_path, config_name, config_attrs)
-                        if config_name in all_configs:
-                            # relax check for the same header/variable in a different tree
-                            if filename != all_configs[config_name]['filename_only']:
+                            #print(file_path, config_name, config_attrs)
+                            all_configs = chips_all_configs[applicable]
+                            if config_name in all_configs:
                                 raise Exception("Found {} at {}:{} but it was already declared at {}:{}".format(config_name, file_path, linenum, os.path.join(scandir, all_configs[config_name]['filename']), all_configs[config_name]['line_number']))
-                        else:
-                            all_configs[config_name] = {'attrs': config_attrs, 'filename': os.path.relpath(file_path, scandir), 'filename_only' : filename, 'line_number': linenum, 'description': config_description}
+                            else:
+                                all_configs[config_name] = {'attrs': config_attrs, 'filename': os.path.relpath(file_path, scandir), 'line_number': linenum, 'description': config_description}
                     else:
                         m = DEFINE_RE.match(line)
                         if m:
@@ -180,6 +204,7 @@ for dirpath, dirnames, filenames in os.walk(scandir):
                                 m = re.match(r'^_u\(((0x)?\d+)\)$', value.lower())
                                 if m:
                                     value = m.group(1)
+                            all_defines = chips_all_defines[applicable]
                             if name not in all_defines:
                                 all_defines[name] = dict()
                             if value not in all_defines[name]:
@@ -187,40 +212,98 @@ for dirpath, dirnames, filenames in os.walk(scandir):
                             all_defines[name][value] = (file_path, linenum)
 
 # Check for defines with missing PICO_CONFIG entries
-resolved_defines = dict()
-for d in all_defines:
-    if d not in all_configs and d.startswith("PICO_"):
-        logger.warning("Potential unmarked PICO define {}".format(d))
-    # resolve "nested defines" - this allows e.g. USB_DPRAM_MAX to resolve to USB_DPRAM_SIZE which is set to 4096 (which then matches the relevant PICO_CONFIG entry)
-    for val in all_defines[d]:
-        if val in all_defines:
-            resolved_defines[d] = all_defines[val]
+chips_resolved_defines = defaultdict(dict)
+for applicable, all_defines in chips_all_defines.items():
+    for d in all_defines:
+        if d not in all_configs and d.startswith("PICO_"):
+            logger.warning("Potential unmarked PICO define {}".format(d))
+        resolved_defines = chips_resolved_defines[applicable]
+        # resolve "nested defines" - this allows e.g. USB_DPRAM_MAX to resolve to USB_DPRAM_SIZE which is set to 4096 (which then matches the relevant PICO_CONFIG entry)
+        for val in all_defines[d]:
+            if val in all_defines:
+                resolved_defines[d] = all_defines[val]
 
-for config_name, config_obj in all_configs.items():
-    file_path = os.path.join(scandir, config_obj['filename'])
-    linenum = config_obj['line_number']
+for applicable, all_configs in chips_all_configs.items():
+    all_defines = chips_all_defines[applicable]
+    for config_name, config_obj in all_configs.items():
+        file_path = os.path.join(scandir, config_obj['filename'])
+        linenum = config_obj['line_number']
 
-    ValidateAttrs(config_obj['attrs'], file_path, linenum)
+        ValidateAttrs(config_obj['attrs'], file_path, linenum)
 
-    # Check that default values match up
-    if 'default' in config_obj['attrs']:
-        config_default = config_obj['attrs']['default']
-        if config_name in all_defines:
-            defines_obj = all_defines[config_name]
-            if config_default not in defines_obj and (config_name not in resolved_defines or config_default not in resolved_defines[config_name]):
-                if '/' in config_default or ' ' in config_default:
-                    continue
-                # There _may_ be multiple matching defines, but arbitrarily display just one in the error message
-                first_define_value = list(defines_obj.keys())[0]
-                first_define_file_path, first_define_linenum = defines_obj[first_define_value]
-                raise Exception('Found {} at {}:{} with a default of {}, but #define says {} (at {}:{})'.format(config_name, file_path, linenum, config_default, first_define_value, first_define_file_path, first_define_linenum))
-        else:
-            raise Exception('Found {} at {}:{} with a default of {}, but no matching #define found'.format(config_name, file_path, linenum, config_default))
+        # Check that default values match up
+        if 'default' in config_obj['attrs']:
+            config_default = config_obj['attrs']['default']
+            if config_name in all_defines:
+                defines_obj = all_defines[config_name]
+                if config_default not in defines_obj and (config_name not in resolved_defines or config_default not in resolved_defines[config_name]):
+                    if '/' in config_default or ' ' in config_default:
+                        continue
+                    # There _may_ be multiple matching defines, but arbitrarily display just one in the error message
+                    first_define_value = list(defines_obj.keys())[0]
+                    first_define_file_path, first_define_linenum = defines_obj[first_define_value]
+                    raise Exception('Found {} at {}:{} with a default of {}, but #define says {} (at {}:{})'.format(config_name, file_path, linenum, config_default, first_define_value, first_define_file_path, first_define_linenum))
+            else:
+                raise Exception('Found {} at {}:{} with a default of {}, but no matching #define found'.format(config_name, file_path, linenum, config_default))
+
+# All settings in "host" should also be in "all"
+for config_name, config_obj in chips_all_configs["host"].items():
+    if config_name not in chips_all_configs["all"]:
+        file_path = os.path.join(scandir, config_obj['filename'])
+        linenum = config_obj['line_number']
+        raise Exception("Found 'host' config {} at {}:{}, but no matching non-host config found".format(config_name, file_path, linenum))
+
+# Any chip-specific settings should not be in "all"
+for chip in CHIP_NAMES:
+    for config_name, chip_config_obj in chips_all_configs[chip].items():
+        if config_name in chips_all_configs["all"]:
+            all_config_obj = chips_all_configs["all"][config_name]
+            chip_file_path = os.path.join(scandir, chip_config_obj['filename'])
+            chip_linenum = chip_config_obj['line_number']
+            all_file_path = os.path.join(scandir, all_config_obj['filename'])
+            all_linenum = all_config_obj['line_number']
+            raise Exception("'{}' config {} at {}:{} also found at {}:{}".format(chip, config_name, chip_file_path, chip_linenum, all_file_path, all_linenum))
+
+def build_mismatch_exception_message(name, thing, config_obj1, value1, config_obj2, value2):
+    obj1_filepath = os.path.join(scandir, config_obj1['filename'])
+    obj2_filepath = os.path.join(scandir, config_obj2['filename'])
+    return "'{}' {} mismatch at {}:{} ({}) and {}:{} ({})".format(name, thing, obj1_filepath, config_obj1['line_number'], value1, obj2_filepath, config_obj2['line_number'], value2)
+
+# Check that any identically-named setttings have appropriate matching attributes
+for applicable in chips_all_configs:
+    for other in chips_all_configs:
+        if other == applicable:
+            continue
+        for config_name, applicable_config_obj in chips_all_configs[applicable].items():
+            if config_name in chips_all_configs[other]:
+                other_config_obj = chips_all_configs[other][config_name]
+                # Check that fields match
+                for field in ['description']:
+                    applicable_value = applicable_config_obj[field]
+                    other_value = other_config_obj[field]
+                    if applicable_value != other_value:
+                        raise Exception(build_mismatch_exception_message(config_name, field, applicable_config_obj, applicable_value, other_config_obj, other_value))
+                # Check that attributes match
+                for attr in applicable_config_obj['attrs']:
+                    if attr != 'default': # totally fine for defaults to vary per-platform
+                        applicable_value = applicable_config_obj['attrs'][attr]
+                        other_value = other_config_obj['attrs'][attr]
+                        if applicable_value != other_value:
+                            raise Exception(build_mismatch_exception_message(config_name, "attribute '{}'".format(attr), applicable_config_obj, applicable_value, other_config_obj, other_value))
+
+# Sort the output alphabetically by name and then by chip
+output_rows = set()
+for chip in (*CHIP_NAMES, "host", "all"):
+    if chip in chips_all_configs:
+        all_configs = chips_all_configs[chip]
+        for config_name in all_configs:
+            output_rows.add((config_name, chip))
 
 with open(outfile, 'w', newline='') as csvfile:
-    fieldnames = ('name', 'location', 'description', 'type') + tuple(sorted(all_attrs - set(['type'])))
+    fieldnames = ('name', 'location', 'platform', 'chip', 'description', 'type') + tuple(sorted(all_attrs - set(['type'])))
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction='ignore', dialect='excel-tab')
 
     writer.writeheader()
-    for config_name, config_obj in sorted(all_configs.items()):
-        writer.writerow({'name': config_name, 'location': '/{}:{}'.format(config_obj['filename'], config_obj['line_number']), 'description': config_obj['description'], **config_obj['attrs']})
+    for config_name, chip in sorted(output_rows):
+        config_obj = chips_all_configs[chip][config_name]
+        writer.writerow({'name': config_name, 'location': '/{}:{}'.format(config_obj['filename'], config_obj['line_number']), 'platform': "host" if chip == "host" else "rp2", 'chip': chip if chip in CHIP_NAMES else "all", 'description': config_obj['description'], **config_obj['attrs']})

cmake/preload/toolchains/util/pico_arm_gcc_common.cmake
@@ -3,7 +3,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/find_compiler.cmake)
 # include our Platform/PICO.cmake
 set(CMAKE_SYSTEM_NAME PICO)
 
-# PICO_CMAKE_CONFIG: PICO_GCC_TRIPLE, List of GCC_TRIPLES -- usually only one -- to try when searching for a compiler. This may be specified the user environment, type=int, default=PICO_DEFAULT_GCC_TRIPLE which is set based on PICO_COMPILER, group=pico_base, doxref=cmake-toolchain-config
+# PICO_CMAKE_CONFIG: PICO_GCC_TRIPLE, List of GCC_TRIPLES -- usually only one -- to try when searching for a compiler. This may be specified the user environment, type=int, default=PICO_DEFAULT_GCC_TRIPLE which is set based on PICO_COMPILER, group=pico_base, docref=cmake-toolchain-config
 if (NOT PICO_GCC_TRIPLE)
     if (DEFINED ENV{_SAVED_PICO_GCC_TRIPLE})
         # saved within the same cmake invocation

src/cmake/on_device.cmake
@@ -63,7 +63,7 @@ function(pico_add_extra_outputs TARGET)
     pico_add_map_output(${TARGET})
 
     # PICO_CMAKE_CONFIG: PICO_NO_TARGET_NAME, Don't define PICO_TARGET_NAME, type=bool, default=0, group=build
-    # PICO_BUILD_DEFINE: PICO_TARGET_NAME, The name of the build target being compiled (unless PICO_NO_TARGET_NAME set in build), type=string, default=target name, group=build
+    # PICO_BUILD_DEFINE: PICO_TARGET_NAME, Name of the build target being compiled (unless PICO_NO_TARGET_NAME set in build), type=string, default=target name, group=build
     if (NOT PICO_NO_TARGET_NAME)
         target_compile_definitions(${TARGET} PRIVATE
                 PICO_TARGET_NAME="${TARGET}"

src/host/hardware_irq/include/hardware/irq.h
@@ -18,7 +18,7 @@
 #define PICO_DISABLE_SHARED_IRQ_HANDLERS 0
 #endif
 
-// PICO_CONFIG: PICO_VTABLE_PER_CORE, user is using separate vector tables per core, type=bool, default=0, group=hardware_irq
+// PICO_CONFIG: PICO_VTABLE_PER_CORE, User is using separate vector tables per core, type=bool, default=0, group=hardware_irq
 #ifndef PICO_VTABLE_PER_CORE
 #define PICO_VTABLE_PER_CORE 0
 #endif

src/host/hardware_sync/include/hardware/sync.h
@@ -26,52 +26,52 @@ static inline void atomic_thread_fence(uint x) {}
 
 #endif
 
-/// PICO_CONFIG: PICO_SPINLOCK_ID_ATOMIC, Spinlock ID for atomic protection, min=0, max=31, default=8, group=hardware_sync
+// PICO_CONFIG: PICO_SPINLOCK_ID_ATOMIC, Spinlock ID for atomics, min=0, max=31, default=8, group=hardware_sync
 #ifndef PICO_SPINLOCK_ID_ATOMIC
 #define PICO_SPINLOCK_ID_ATOMIC 8
 #endif
 
-/// PICO_CONFIG: PICO_SPINLOCK_ID_IRQ, Spinlock ID for IRQ protection, min=0, max=31, default=9, group=hardware_sync
+// PICO_CONFIG: PICO_SPINLOCK_ID_IRQ, Spinlock ID for IRQ protection, min=0, max=31, default=9, group=hardware_sync
 #ifndef PICO_SPINLOCK_ID_IRQ
 #define PICO_SPINLOCK_ID_IRQ 9
 #endif
 
-/// PICO_CONFIG: PICO_SPINLOCK_ID_TIMER, Spinlock ID for Timer protection, min=0, max=31, default=10, group=hardware_sync
+// PICO_CONFIG: PICO_SPINLOCK_ID_TIMER, Spinlock ID for Timer protection, min=0, max=31, default=10, group=hardware_sync
 #ifndef PICO_SPINLOCK_ID_TIMER
 #define PICO_SPINLOCK_ID_TIMER 10
 #endif
 
-/// PICO_CONFIG: PICO_SPINLOCK_ID_HARDWARE_CLAIM, Spinlock ID for Hardware claim protection, min=0, max=31, default=11, group=hardware_sync
+// PICO_CONFIG: PICO_SPINLOCK_ID_HARDWARE_CLAIM, Spinlock ID for Hardware claim protection, min=0, max=31, default=11, group=hardware_sync
 #ifndef PICO_SPINLOCK_ID_HARDWARE_CLAIM
 #define PICO_SPINLOCK_ID_HARDWARE_CLAIM 11
 #endif
 
-/// PICO_CONFIG: PICO_SPINLOCK_ID_RAND, Spinlock ID for Random Number Generator, min=0, max=31, default=12, group=hardware_sync
+// PICO_CONFIG: PICO_SPINLOCK_ID_RAND, Spinlock ID for Random Number Generator, min=0, max=31, default=12, group=hardware_sync
 #ifndef PICO_SPINLOCK_ID_RAND
 #define PICO_SPINLOCK_ID_RAND 12
 #endif
 
-/// PICO_CONFIG: PICO_SPINLOCK_ID_OS1, First Spinlock ID reserved for use by low level OS style software, min=0, max=31, default=14, group=hardware_sync
+// PICO_CONFIG: PICO_SPINLOCK_ID_OS1, First Spinlock ID reserved for use by low level OS style software, min=0, max=31, default=14, group=hardware_sync
 #ifndef PICO_SPINLOCK_ID_OS1
 #define PICO_SPINLOCK_ID_OS1 14
 #endif
 
-/// PICO_CONFIG: PICO_SPINLOCK_ID_OS2, Second Spinlock ID reserved for use by low level OS style software, min=0, max=31, default=15, group=hardware_sync
+// PICO_CONFIG: PICO_SPINLOCK_ID_OS2, Second Spinlock ID reserved for use by low level OS style software, min=0, max=31, default=15, group=hardware_sync
 #ifndef PICO_SPINLOCK_ID_OS2
 #define PICO_SPINLOCK_ID_OS2 15
 #endif
 
-/// PICO_CONFIG: PICO_SPINLOCK_ID_STRIPED_FIRST, Lowest Spinlock ID in the 'striped' range, min=0, max=31, default=16, group=hardware_sync
+// PICO_CONFIG: PICO_SPINLOCK_ID_STRIPED_FIRST, Lowest Spinlock ID in the 'striped' range, min=0, max=31, default=16, group=hardware_sync
 #ifndef PICO_SPINLOCK_ID_STRIPED_FIRST
 #define PICO_SPINLOCK_ID_STRIPED_FIRST 16
 #endif
 
-/// PICO_CONFIG: PICO_SPINLOCK_ID_STRIPED_LAST, Highest Spinlock ID in the 'striped' range, min=0, max=31, default=23, group=hardware_sync
+// PICO_CONFIG: PICO_SPINLOCK_ID_STRIPED_LAST, Highest Spinlock ID in the 'striped' range, min=0, max=31, default=23, group=hardware_sync
 #ifndef PICO_SPINLOCK_ID_STRIPED_LAST
 #define PICO_SPINLOCK_ID_STRIPED_LAST 23
 #endif
 
-/// PICO_CONFIG: PICO_SPINLOCK_ID_CLAIM_FREE_FIRST, Lowest Spinlock ID in the 'claim free' range, min=0, max=31, default=24, group=hardware_sync
+// PICO_CONFIG: PICO_SPINLOCK_ID_CLAIM_FREE_FIRST, Lowest Spinlock ID in the 'claim free' range, min=0, max=31, default=24, group=hardware_sync
 #ifndef PICO_SPINLOCK_ID_CLAIM_FREE_FIRST
 #define PICO_SPINLOCK_ID_CLAIM_FREE_FIRST 24
 #endif
@@ -80,7 +80,7 @@ static inline void atomic_thread_fence(uint x) {}
 #warning PICO_SPINLOCK_ID_CLAIM_FREE_END has been renamed to PICO_SPINLOCK_ID_CLAIM_FREE_LAST
 #endif
 
-/// PICO_CONFIG: PICO_SPINLOCK_ID_CLAIM_FREE_LAST, Highest Spinlock ID in the 'claim free' range, min=0, max=31, default=31, group=hardware_sync
+// PICO_CONFIG: PICO_SPINLOCK_ID_CLAIM_FREE_LAST, Highest Spinlock ID in the 'claim free' range, min=0, max=31, default=31, group=hardware_sync
 #ifndef PICO_SPINLOCK_ID_CLAIM_FREE_LAST
 #define PICO_SPINLOCK_ID_CLAIM_FREE_LAST 31
 #endif

src/rp2040/boot_stage2/include/boot_stage2/config.h
@@ -11,7 +11,7 @@
 
 #include "pico.h"
 
-// PICO_CONFIG: PICO_BUILD_BOOT_STAGE2_NAME, The name of the boot stage 2 if selected by the build, group=boot_stage2
+// PICO_CONFIG: PICO_BUILD_BOOT_STAGE2_NAME, Name of the boot stage 2 if selected in the build system, group=boot_stage2
 #ifdef PICO_BUILD_BOOT_STAGE2_NAME
     #define _BOOT_STAGE2_SELECTED
 #else

src/rp2040/pico_platform/include/pico/platform.h
@@ -66,11 +66,6 @@
 #define PICO_RP2040_B2_SUPPORTED 1
 #endif
 
-// PICO_CONFIG: PICO_RP2350_A2_SUPPORTED, Whether to include any specific software support for RP2350 A2 revision, type=bool, default=1, advanced=true, group=pico_platform
-#ifndef PICO_RP2350_A2_SUPPORTED
-#define PICO_RP2350_A2_SUPPORTED 1
-#endif
-
 #ifndef PICO_RAM_VECTOR_TABLE_SIZE
 #define PICO_RAM_VECTOR_TABLE_SIZE (VTABLE_FIRST_IRQ + NUM_IRQS)
 #endif

src/rp2350/hardware_regs/include/hardware/platform_defs.h
@@ -74,7 +74,7 @@
 #define HAS_RP2350_TRNG 1
 #define HAS_HSTX 1
 
-// PICO_CONFIG: XOSC_HZ, The crystal oscillator frequency in Hz, type=int, default=12000000, advanced=true, group=hardware_base
+// PICO_CONFIG: XOSC_HZ, Crystal oscillator frequency in Hz, type=int, default=12000000, advanced=true, group=hardware_base
 // NOTE:  The system and USB clocks are generated from the frequency using two PLLs.
 // If you override this define, or SYS_CLK_HZ/USB_CLK_HZ below, you will *also* need to add your own adjusted PLL set-up defines to
 // override the defaults which live in src/rp2_common/hardware_clocks/include/hardware/clocks.h
@@ -89,7 +89,7 @@
 #endif
 #endif
 
-// PICO_CONFIG: SYS_CLK_HZ, The system operating frequency in Hz, type=int, default=150000000, advanced=true, group=hardware_base
+// PICO_CONFIG: SYS_CLK_HZ, System operating frequency in Hz, type=int, default=150000000, advanced=true, group=hardware_base
 #ifndef SYS_CLK_HZ
 #ifdef SYS_CLK_KHZ
 #define SYS_CLK_HZ ((SYS_CLK_KHZ) * _u(1000))

commit e2499d1d23b3a69669e4c5947462f42a45a71cbb

Fix bug in extract_configs.py
This bug was actually introduced by #1845 , but I never noticed until now because it depended on which order the filesystem traversed the directories

tools/extract_configs.py
@@ -241,6 +241,7 @@ for applicable, all_defines in chips_all_defines.items():
 
 for applicable, all_configs in chips_all_configs.items():
     all_defines = chips_all_defines[applicable]
+    resolved_defines = chips_resolved_defines[applicable]
     for config_name, config_obj in all_configs.items():
         file_path = os.path.join(scandir, config_obj['filename'])
         linenum = config_obj['line_number']