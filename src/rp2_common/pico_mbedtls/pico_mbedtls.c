4cacd246abae8467cec1eb77255f1b5753e1adb9
Peter Harper <peter.harper@raspberrypi.com>
2025-04-08 13:00:28 +0100

    Add missing config docs for overriding libs
    Adds some docs for pico_mbedtls
    Documents the config for overriding the code in pico-sdk/libs

--- src/rp2_common/pico_mbedtls/pico_mbedtls.c
+++ src/rp2_common/pico_mbedtls/pico_mbedtls.c
@@ -27,7 +27,7 @@ int mbedtls_hardware_poll(void *data __unused, unsigned char *output, size_t len
 #error SHA256 hardware acceleration not supported
 #endif
 
-// PICO_CONFIG: PICO_MBEDTLS_SHA256_ALT_USE_DMA, Whether to use DMA for writing to hardware for the mbedtls SHA-256 hardware acceleration, type=int, default=1, group=pico_stdlib
+// PICO_CONFIG: PICO_MBEDTLS_SHA256_ALT_USE_DMA, Whether to use DMA for writing to hardware for the mbedtls SHA-256 hardware acceleration, type=int, default=1, group=pico_mbedtls
 #ifndef PICO_MBEDTLS_SHA256_ALT_USE_DMA
 #define PICO_MBEDTLS_SHA256_ALT_USE_DMA 1
 #endif
