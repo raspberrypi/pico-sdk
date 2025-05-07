5b0fa4bc823755926d11ac66eba09fae03d7119b
Sergio R. Caprile <scaprile@gmail.com>
2025-05-07 13:11:06 -0400

    update to new syntax
    Signed-off-by: Sergio R. Caprile <scaprile@gmail.com>

--- src/boards/include/boards/wiznet_w5100s_evb_pico2.h
+++ src/boards/include/boards/wiznet_w5100s_evb_pico2.h
@@ -9,10 +9,10 @@
 //       SHOULD ONLY CONSIST OF PREPROCESSOR DIRECTIVES
 // -----------------------------------------------------
 
-// pico_cmake_set PICO_PLATFORM=rp2350
-
 #ifndef _BOARDS_WIZNET_W5100S_EVB_PICO2_H
 #define _BOARDS_WIZNET_W5100S_EVB_PICO2_H
+
+pico_board_cmake_set(PICO_PLATFORM, rp2350)
 
 // For board detection
 #define WIZNET_W5100S_EVB_PICO2
@@ -89,7 +89,7 @@
 #define PICO_FLASH_SPI_CLKDIV 2
 #endif
 
-// pico_cmake_set_default PICO_FLASH_SIZE_BYTES = (2 * 1024 * 1024)
+pico_board_cmake_set_default(PICO_FLASH_SIZE_BYTES, (2 * 1024 * 1024))
 #ifndef PICO_FLASH_SIZE_BYTES
 #define PICO_FLASH_SIZE_BYTES (2 * 1024 * 1024)
 #endif
@@ -107,7 +107,7 @@
 #define PICO_VSYS_PIN 29
 #endif
 
-// pico_cmake_set_default PICO_RP2350_A2_SUPPORTED = 1
+pico_board_cmake_set_default(PICO_RP2350_A2_SUPPORTED, 1)
 #ifndef PICO_RP2350_A2_SUPPORTED
 #define PICO_RP2350_A2_SUPPORTED 1
 #endif
