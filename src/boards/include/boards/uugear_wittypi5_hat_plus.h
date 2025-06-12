99bd574b26d947610b7f1991dab831a9c93a5bb0
UUGear <shawn@uugear.com>
2025-06-12 12:24:34 +0200

    Update uugear_wittypi5_hat_plus.h
    Fixes detected errors

--- src/boards/include/boards/uugear_wittypi5_hat_plus.h
+++ src/boards/include/boards/uugear_wittypi5_hat_plus.h
@@ -11,10 +11,10 @@
 
 // This header may be included by other board headers as "boards/uugear_wittypi5_hat_plus.h"
 
-// pico_cmake_set PICO_PLATFORM=rp2350
-
 #ifndef _BOARDS_UUGEAR_WITTYPI5_HAT_PLUS_H
 #define _BOARDS_UUGEAR_WITTYPI5_HAT_PLUS_H
+
+pico_board_cmake_set(PICO_PLATFORM, rp2350)
 
 // For board detection
 #define UUGEAR_WITTYPI5_HAT_PLUS
@@ -50,19 +50,18 @@
 #endif
 
 // --- FLASH ---
-
 #define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
 
 #ifndef PICO_FLASH_SPI_CLKDIV
 #define PICO_FLASH_SPI_CLKDIV 2
 #endif
 
-// pico_cmake_set_default PICO_FLASH_SIZE_BYTES = (16 * 1024 * 1024)
+pico_board_cmake_set_default(PICO_FLASH_SIZE_BYTES, (16 * 1024 * 1024))
 #ifndef PICO_FLASH_SIZE_BYTES
 #define PICO_FLASH_SIZE_BYTES (16 * 1024 * 1024)
 #endif
 
-// pico_cmake_set_default PICO_RP2350_A2_SUPPORTED = 1
+pico_board_cmake_set_default(PICO_RP2350_A2_SUPPORTED, 1)
 #ifndef PICO_RP2350_A2_SUPPORTED
 #define PICO_RP2350_A2_SUPPORTED 1
 #endif
