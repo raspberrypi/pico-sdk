0a2fbb0e3a0eadea634c122dd46120dac6faa557
Peter Harper <peter.harper@raspberrypi.com>
2025-06-18 18:34:40 +0100

    Allow logging to be overridden

--- src/rp2_common/pico_cyw43_driver/cybt_shared_bus/cybt_logging.h
+++ src/rp2_common/pico_cyw43_driver/cybt_shared_bus/cybt_logging.h
@@ -8,42 +8,50 @@
 #define CYBT_LOGGING_H
 
 // Error messages only enabled in debug by default
+#ifndef CYBT_ERROR_ENABLED
 #ifndef NDEBUG
 #define CYBT_ERROR_ENABLED 1
 #else
 #define CYBT_ERROR_ENABLED 0
 #endif
+#endif
 
 // Info messages only enabled in debug by default
+#ifndef CYBT_INFO_ENABLED
 #ifndef NDEBUG
 #define CYBT_INFO_ENABLED 1
 #else
 #define CYBT_INFO_ENABLED 0
 #endif
-
-// Debug messages disabled by default
-#define CYBT_DEBUG_ENABLED 0
-
-#if CYBT_DEBUG_ENABLED || CYBT_ERROR_ENABLED
-#include <stdio.h>
 #endif
 
+// Debug messages disabled by default
+#ifndef CYBT_DEBUG_ENABLED
+#define CYBT_DEBUG_ENABLED 0
+#endif
+
+#ifndef cybt_error
 #if CYBT_ERROR_ENABLED
 #define cybt_error CYW43_WARN
 #else
 #define cybt_error(...)
 #endif
+#endif
 
+#ifndef cybt_info
 #if CYBT_INFO_ENABLED
 #define cybt_info CYW43_PRINTF
 #else
 #define cybt_info(...)
 #endif
+#endif
 
+#ifndef cybt_debug
 #if CYBT_DEBUG_ENABLED
 #define cybt_debug CYW43_PRINTF
 #else
 #define cybt_debug(...)
+#endif
 #endif
 
 #endif // CYBT_LOGGING_H
