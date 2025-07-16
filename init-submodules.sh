c96dfd8845ad9065d9bc8effa1d4fba8ec443b9c
graham sanderson <graham.sanderson@raspberrypi.com>
2025-07-16 17:04:21 -0500

    .

---
+++ init-submodules.sh
@@ -0,0 +1,6 @@
+git submodule sync
+git submodule update --init lib/btstack
+git submodule update --init lib/cyw43-driver
+git submodule update --init lib/lwip
+git submodule update --init lib/mbedtls
+git submodule update --init lib/tinyusb
