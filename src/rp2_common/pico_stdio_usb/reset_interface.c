186c37de9fc1094195c21e72f84e7d6036623446
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2025-08-06 16:32:40 +0100

    Create separate library for reset interface

--- src/rp2_common/pico_stdio_usb/reset_interface.c
+++ src/rp2_common/pico_stdio_usb/reset_interface.c
@@ -15,7 +15,6 @@
 
 #if PICO_STDIO_USB_ENABLE_RESET_VIA_VENDOR_INTERFACE
 #include "hardware/watchdog.h"
-#include "device/usbd_pvt.h"
 
 static uint8_t itf_num;
 
@@ -88,14 +87,14 @@ bool tud_vendor_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_requ
 }
 #endif
 
-static void resetd_init(void) {
+void resetd_init(void) {
 }
 
-static void resetd_reset(uint8_t __unused rhport) {
+void resetd_reset(uint8_t __unused rhport) {
     itf_num = 0;
 }
 
-static uint16_t resetd_open(uint8_t __unused rhport, tusb_desc_interface_t const *itf_desc, uint16_t max_len) {
+uint16_t resetd_open(uint8_t __unused rhport, tusb_desc_interface_t const *itf_desc, uint16_t max_len) {
     TU_VERIFY(TUSB_CLASS_VENDOR_SPECIFIC == itf_desc->bInterfaceClass &&
               RESET_INTERFACE_SUBCLASS == itf_desc->bInterfaceSubClass &&
               RESET_INTERFACE_PROTOCOL == itf_desc->bInterfaceProtocol, 0);
@@ -108,7 +107,7 @@ static uint16_t resetd_open(uint8_t __unused rhport, tusb_desc_interface_t const
 }
 
 // Support for parameterized reset via vendor interface control request
-static bool resetd_control_xfer_cb(uint8_t __unused rhport, uint8_t stage, tusb_control_request_t const * request) {
+bool resetd_control_xfer_cb(uint8_t __unused rhport, uint8_t stage, tusb_control_request_t const * request) {
     // nothing to do with DATA & ACK stage
     if (stage != CONTROL_STAGE_SETUP) return true;
 
@@ -145,28 +144,17 @@ static bool resetd_control_xfer_cb(uint8_t __unused rhport, uint8_t stage, tusb_
     return false;
 }
 
-static bool resetd_xfer_cb(uint8_t __unused rhport, uint8_t __unused ep_addr, xfer_result_t __unused result, uint32_t __unused xferred_bytes) {
+bool resetd_xfer_cb(uint8_t __unused rhport, uint8_t __unused ep_addr, xfer_result_t __unused result, uint32_t __unused xferred_bytes) {
     return true;
 }
 
-static usbd_class_driver_t const _resetd_driver =
-{
-#if CFG_TUSB_DEBUG >= 2
-    .name = "RESET",
-#endif
-    .init             = resetd_init,
-    .reset            = resetd_reset,
-    .open             = resetd_open,
-    .control_xfer_cb  = resetd_control_xfer_cb,
-    .xfer_cb          = resetd_xfer_cb,
-    .sof              = NULL
-};
-
+#if PICO_STDIO_USB_RESET_INCLUDE_APP_DRIVER_CB
 // Implement callback to add our custom driver
 usbd_class_driver_t const *usbd_app_driver_get_cb(uint8_t *driver_count) {
     *driver_count = 1;
     return &_resetd_driver;
 }
+#endif
 #endif
 
 #if PICO_STDIO_USB_ENABLE_RESET_VIA_BAUD_RATE
