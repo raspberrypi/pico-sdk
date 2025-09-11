398b71e9f6e91c87dea969dde2b234ba0c65fa9e
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2025-09-11 14:09:20 +0100

    Add support for xip_sram binaries on RP2040 and RP2350
    On RP2040 they are copy to xip sram, as the bootrom doesn't support direct entry into xip sram

--- test/pico_xip_sram_test/pico_xip_sram_test.c
+++ test/pico_xip_sram_test/pico_xip_sram_test.c
@@ -5,4 +5,12 @@
 int main(void) {
     stdio_init_all();
     printf("pico_xip_sram_test begins\n");
+
+    for (int i = 0; i < 5; i++) {
+        printf("running... %d\n", i);
+        sleep_ms(500);
+    }
+
+    printf("pico_xip_sram_test ends\n");
+    return 0;
 }
