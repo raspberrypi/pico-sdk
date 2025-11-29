34e8de76fe0c4c9f787773a905c46aec62764c66
Richard Hulme <r_hulme@yahoo.com>
2025-11-29 13:10:24 +0100

    Mark all parameters in host GPIO unused
    This prevents lots of compiler warnings if the default warning level
    is increased.
    
    Refs #2736

--- src/host/hardware_gpio/gpio.c
+++ src/host/hardware_gpio/gpio.c
@@ -7,97 +7,97 @@
 #include "hardware/gpio.h"
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_function)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_function)(uint gpio, enum gpio_function fn) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_function)(__unused uint gpio, __unused enum gpio_function fn) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_pull_up)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_pull_up)(uint gpio) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_pull_up)(__unused uint gpio) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_pull_down)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_pull_down)(uint gpio) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_pull_down)(__unused uint gpio) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_disable_pulls)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_disable_pulls)(uint gpio) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_disable_pulls)(__unused uint gpio) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_pulls)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_pulls)(uint gpio, bool up, bool down) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_pulls)(__unused uint gpio, __unused bool up, __unused bool down) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_irqover)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_irqover)(uint gpio, uint value) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_irqover)(__unused uint gpio, __unused uint value) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_outover)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_outover)(uint gpio, uint value) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_outover)(__unused uint gpio, __unused uint value) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_inover)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_inover)(uint gpio, uint value) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_inover)(__unused uint gpio, __unused uint value) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_oeover)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_oeover)(uint gpio, uint value) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_oeover)(__unused uint gpio, __unused uint value) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_input_hysteresis_enabled)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_input_hysteresis_enabled)(uint gpio, bool enabled){
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_input_hysteresis_enabled)(__unused uint gpio, __unused bool enabled){
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_is_input_hysteresis_enabled)
-bool PICO_WEAK_FUNCTION_IMPL_NAME(gpio_is_input_hysteresis_enabled)(uint gpio){
+bool PICO_WEAK_FUNCTION_IMPL_NAME(gpio_is_input_hysteresis_enabled)(__unused uint gpio){
     return true;
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_slew_rate)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_slew_rate)(uint gpio, enum gpio_slew_rate slew){
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_slew_rate)(__unused uint gpio, __unused enum gpio_slew_rate slew){
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_get_slew_rate)
-enum gpio_slew_rate PICO_WEAK_FUNCTION_IMPL_NAME(gpio_get_slew_rate)(uint gpio){
+enum gpio_slew_rate PICO_WEAK_FUNCTION_IMPL_NAME(gpio_get_slew_rate)(__unused uint gpio){
     return GPIO_SLEW_RATE_FAST;
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_drive_strength)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_drive_strength)(uint gpio, enum gpio_drive_strength drive){
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_drive_strength)(__unused uint gpio, __unused enum gpio_drive_strength drive){
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_get_drive_strength)
-enum gpio_drive_strength PICO_WEAK_FUNCTION_IMPL_NAME(gpio_get_drive_strength)(uint gpio){
+enum gpio_drive_strength PICO_WEAK_FUNCTION_IMPL_NAME(gpio_get_drive_strength)(__unused uint gpio){
     return GPIO_DRIVE_STRENGTH_4MA;
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_irq_enabled)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_irq_enabled)(uint gpio, uint32_t events, bool enable) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_irq_enabled)(__unused uint gpio, __unused uint32_t events, __unused bool enable) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_acknowledge_irq)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_acknowledge_irq)(uint gpio, uint32_t events) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_acknowledge_irq)(__unused uint gpio, __unused uint32_t events) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_init)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_init)(uint gpio) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_init)(__unused uint gpio) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_get)
-bool PICO_WEAK_FUNCTION_IMPL_NAME(gpio_get)(uint gpio) {
+bool PICO_WEAK_FUNCTION_IMPL_NAME(gpio_get)(__unused uint gpio) {
     return 0;
 }
 
@@ -107,57 +107,57 @@ uint32_t PICO_WEAK_FUNCTION_IMPL_NAME(gpio_get_all)() {
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_mask)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_mask)(uint32_t mask) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_mask)(__unused uint32_t mask) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_clr_mask)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_clr_mask)(uint32_t mask) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_clr_mask)(__unused uint32_t mask) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_xor_mask)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_xor_mask)(uint32_t mask) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_xor_mask)(__unused uint32_t mask) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_put_masked)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_put_masked)(uint32_t mask, uint32_t value) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_put_masked)(__unused uint32_t mask, __unused uint32_t value) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_put_all)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_put_all)(uint32_t value) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_put_all)(__unused uint32_t value) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_put)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_put)(uint gpio, int value) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_put)(__unused uint gpio, __unused int value) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_dir_out_masked)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_dir_out_masked)(uint32_t mask) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_dir_out_masked)(__unused uint32_t mask) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_dir_in_masked)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_dir_in_masked)(uint32_t mask) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_dir_in_masked)(__unused uint32_t mask) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_dir_masked)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_dir_masked)(uint32_t mask, uint32_t value) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_dir_masked)(__unused uint32_t mask, __unused uint32_t value) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_dir_all_bits)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_dir_all_bits)(uint32_t value) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_dir_all_bits)(__unused uint32_t value) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_dir)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_dir)(uint gpio, bool out) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_dir)(__unused uint gpio, __unused bool out) {
 
 }
 
@@ -167,11 +167,11 @@ void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_debug_pins_init)() {
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_set_input_enabled)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_input_enabled)(uint gpio, bool enable) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_set_input_enabled)(__unused uint gpio, __unused bool enable) {
 
 }
 
 PICO_WEAK_FUNCTION_DEF(gpio_init_mask)
-void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_init_mask)(uint gpio_mask) {
+void PICO_WEAK_FUNCTION_IMPL_NAME(gpio_init_mask)(__unused uint gpio_mask) {
 
 }
