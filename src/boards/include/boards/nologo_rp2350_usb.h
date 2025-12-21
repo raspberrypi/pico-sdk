b1ab7da1fe26cce33db2d76887126f4849b39e1f
Disappear9 <Disappear9@users.noreply.github.com>
2025-12-21 17:40:34 +0800

    Update nologo_rp2350_usb.h

--- src/boards/include/boards/nologo_rp2350_usb.h
+++ src/boards/include/boards/nologo_rp2350_usb.h
@@ -38,7 +38,7 @@ pico_board_cmake_set(PICO_PLATFORM, rp2350)
 
 // --- I2C ---
 #ifndef PICO_DEFAULT_I2C
-#define PICO_DEFAULT_I2C 1
+#define PICO_DEFAULT_I2C 0
 #endif
 #ifndef PICO_DEFAULT_I2C_SDA_PIN
 #define PICO_DEFAULT_I2C_SDA_PIN 16
