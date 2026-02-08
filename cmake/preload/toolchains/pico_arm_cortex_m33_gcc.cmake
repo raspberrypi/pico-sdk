97267d6aee142a3ddc26b240b737a2de46db509d
graham sanderson <graham.sanderson@raspberrypi.com>
2026-02-07 19:04:03 -0600

    Add (non google) LLVM support, and replace calling convention check with #if defined(__ARM_PCS_VFP)

--- cmake/preload/toolchains/pico_arm_cortex_m33_gcc.cmake
+++ cmake/preload/toolchains/pico_arm_cortex_m33_gcc.cmake
@@ -2,12 +2,13 @@ set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
 set(PICO_DEFAULT_GCC_TRIPLE arm-none-eabi)
 
 set(PICO_COMMON_LANG_FLAGS " -mcpu=cortex-m33 -mthumb -march=armv8-m.main+fp+dsp")
-# PICO_CMAKE_CONFIG: PICO_USE_HARDFP_ABI, Enable hard fp abi, type=bool, default=0, group=build, docref=cmake-toolchain-config
-if (PICO_USE_HARD_ABI)
+# PICO_CMAKE_CONFIG: PICO_HARD_FLOAT_ABI, use hard floating point ABI, type=bool, default=0, group=build, docref=cmake-toolchain-config
+if (PICO_HARD_FLOAT_ABI)
     set(PICO_COMMON_LANG_FLAGS "${PICO_COMMON_LANG_FLAGS} -mfloat-abi=hard")
-else ()
+else()
     set(PICO_COMMON_LANG_FLAGS "${PICO_COMMON_LANG_FLAGS} -mfloat-abi=softfp")
-endif ()
+endif()
+
 # PICO_CMAKE_CONFIG: PICO_NO_CMSE, Disable CMSE compiler extensions, type=bool, default=0, group=build, docref=cmake-toolchain-config
 if (NOT PICO_NO_CMSE)
     set(PICO_COMMON_LANG_FLAGS "${PICO_COMMON_LANG_FLAGS} -mcmse")
