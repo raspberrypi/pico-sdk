1bcfac30c13149e4cab262e2c246d5f5471cdc35
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2026-02-11 18:57:12 +0000

    Add pico_add_linker_script_override_path to make overriding individual files easier
    Restructured so that it includes the platform-specific files before common ones, so common ones can be overridden

--- test/kitchen_sink/kitchen_sink.c
+++ test/kitchen_sink/kitchen_sink.c
@@ -113,6 +113,18 @@ int main(void) {
     }
 #endif
 
+#ifdef EXTRA_DATA_SECTION
+    static int extra_data __attribute__((section(".extra_data"))) = 12345678;
+    printf("extra_data before load = %d\n", extra_data);
+
+    extern uint32_t __extra_data_source__;
+    extern uint32_t __extra_data_start__;
+    extern uint32_t __extra_data_end__;
+    uint32_t stored_words = (uint32_t)(&__extra_data_end__ - &__extra_data_start__);
+    memcpy(&__extra_data_start__, &__extra_data_source__, 4 * stored_words);
+
+    printf("extra_data after load = %d\n", extra_data);
+#endif
 #ifndef __riscv
     // this should compile as we are Cortex M0+
     pico_default_asm ("SVC #3");
