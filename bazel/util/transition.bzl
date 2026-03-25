a26a42b1dbee1d1df72546ac85a4b60c4e13d885
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2026-03-25 10:39:28 +0000

    Add azel pico_set_binary_type transition, to allow setting binary type for individual binaries
    Can remove full no_flash etc test builds, as there can now be kitchen_sink_no_flash etc builds like in CMake

--- bazel/util/transition.bzl
+++ bazel/util/transition.bzl
@@ -167,3 +167,18 @@ extra_copts_for_all_deps = declare_transtion(
         "//command_line_option:copt": "extra_copts",
     },
 )
+
+# This transition sets the binary type
+pico_set_binary_type = declare_transtion(
+    attrs = {
+        "binary_type": attr.string(),
+        # This could be shared, but we don't in order to make it clearer that
+        # a transition is in use.
+        "_allowlist_function_transition": attr.label(
+            default = "@bazel_tools//tools/allowlists/function_transition_allowlist",
+        ),
+    },
+    flag_overrides = {
+        "@pico-sdk//bazel/config:PICO_DEFAULT_BINARY_TYPE": "binary_type",
+    },
+)
--- tools/bazel_build.py
+++ tools/bazel_build.py
@@ -37,6 +37,9 @@ BUILD_CONFIGURATIONS = (
                 "//test/hardware_sync_spin_lock_test:hardware_sync_spin_lock_test",
                 "//test/kitchen_sink:kitchen_sink",
                 "//test/kitchen_sink:kitchen_sink_cpp",
+                "//test/kitchen_sink:kitchen_sink_copy_to_ram",
+                "//test/kitchen_sink:kitchen_sink_no_flash",
+                "//test/kitchen_sink:kitchen_sink_blocked_ram",
                 "//test/kitchen_sink:kitchen_sink_lwip_poll",
                 "//test/kitchen_sink:kitchen_sink_lwip_background",
                 "//test/pico_divider_test:pico_divider_test",
@@ -84,6 +87,8 @@ BUILD_CONFIGURATIONS = (
                 "//test/kitchen_sink:kitchen_sink_lwip_background",
                 # Host only.
                 "//test/pico_float_test:hazard3_test_gen",
+                # RP2040 only
+                "//test/kitchen_sink:kitchen_sink_blocked_ram",
                 # TODO: RISC-V support.
                 "//test/pico_float_test:pico_float_test_hazard3",
             )
@@ -122,6 +127,8 @@ BUILD_CONFIGURATIONS = (
                 "//test/kitchen_sink:kitchen_sink_lwip_background",
                 # Host only.
                 "//test/pico_float_test:hazard3_test_gen",
+                # RP2040 only
+                "//test/kitchen_sink:kitchen_sink_blocked_ram",
                 # TODO: RISC-V support.
                 "//test/pico_float_test:pico_float_test_hazard3",
             )
@@ -156,70 +163,8 @@ BUILD_CONFIGURATIONS = (
             (
                 # Host only.
                 "//test/pico_float_test:hazard3_test_gen",
-                # No RISC-V on RP2040.
-                "//test/pico_float_test:pico_float_test_hazard3",
-            )
-        ),
-    },
-    {
-        "name": "rp2040 no_flash",
-        "args": ("--platforms=//bazel/platform:rp2040", "--@pico-sdk//bazel/config:PICO_DEFAULT_BINARY_TYPE=no_flash"),
-        "extra_targets": (),
-        "exclusions": frozenset(
-            (
-                "//test/kitchen_sink:kitchen_sink_lwip_poll",
-                "//test/kitchen_sink:kitchen_sink_lwip_background",
-                # Host only.
-                "//test/pico_float_test:hazard3_test_gen",
-                # No RISC-V on RP2040.
-                "//test/pico_float_test:pico_float_test_hazard3",
-                # hardware_sha256 doesn't appear to work on RP2040.
-                "//test/pico_sha256_test:pico_sha256_test",
-            )
-        ),
-    },
-    {
-        "name": "rp2350 no_flash",
-        "args": ("--platforms=//bazel/platform:rp2350", "--@pico-sdk//bazel/config:PICO_DEFAULT_BINARY_TYPE=no_flash"),
-        "extra_targets": (),
-        "exclusions": frozenset(
-            (
-                "//test/kitchen_sink:kitchen_sink_lwip_poll",
-                "//test/kitchen_sink:kitchen_sink_lwip_background",
-                # Host only.
-                "//test/pico_float_test:hazard3_test_gen",
-                # TODO: RISC-V support.
-                "//test/pico_float_test:pico_float_test_hazard3",
-            )
-        ),
-    },
-    {
-        "name": "rp2040 copy_to_ram",
-        "args": ("--platforms=//bazel/platform:rp2040", "--@pico-sdk//bazel/config:PICO_DEFAULT_BINARY_TYPE=copy_to_ram"),
-        "extra_targets": (),
-        "exclusions": frozenset(
-            (
-                "//test/kitchen_sink:kitchen_sink_lwip_poll",
-                "//test/kitchen_sink:kitchen_sink_lwip_background",
-                # Host only.
-                "//test/pico_float_test:hazard3_test_gen",
-                # No RISC-V on RP2040.
-                "//test/pico_float_test:pico_float_test_hazard3",
-                # hardware_sha256 doesn't appear to work on RP2040.
-                "//test/pico_sha256_test:pico_sha256_test",
-            )
-        ),
-    },
-    {
-        "name": "rp2350 copy_to_ram",
-        "args": ("--platforms=//bazel/platform:rp2350", "--@pico-sdk//bazel/config:PICO_DEFAULT_BINARY_TYPE=copy_to_ram"),
-        "extra_targets": (),
-        "exclusions": frozenset(
-            (
-                "//test/kitchen_sink:kitchen_sink_lwip_poll",
-                "//test/kitchen_sink:kitchen_sink_lwip_background",
-                # Host only.
-                "//test/pico_float_test:hazard3_test_gen",
+                # RP2040 only
+                "//test/kitchen_sink:kitchen_sink_blocked_ram",
                 # TODO: RISC-V support.
                 "//test/pico_float_test:pico_float_test_hazard3",
             )
