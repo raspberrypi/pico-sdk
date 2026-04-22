61f1e455b34e1cd4ad03c3e3a9c398f9bf13d891
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2026-04-22 12:45:34 +0100

    Add more sizes to fixed_bitset_test
    Add 4 and 77 sized bitsets

--- test/fixed_bitset_test/fixed_bitset_test.c
+++ test/fixed_bitset_test/fixed_bitset_test.c
@@ -11,38 +11,46 @@
 
 #define CHECK(b, message, ...) ({ if (!(b)) { printf("FAILED: " message "\n", ##__VA_ARGS__); exit(1); } })
 
+typedef fixed_bitset_type(4) bitset4_t;
 typedef fixed_bitset_type(47) bitset47_t;
+typedef fixed_bitset_type(77) bitset77_t;
 
 int main() {
     stdio_init_all();
 
-    bitset47_t b47;
-    fixed_bitset_init(&b47, bitset47_t, 47, 0);
-    for (int i=0;i<47;i++) {
-        CHECK(!fixed_bitset_get(&b47.bitset, i), "Bit %d should be clear", i);
-    }
+#define FIXED_BITSET_TESTS(name, type, n)\
+    type name;\
+    fixed_bitset_init(&name, type, n, 0);\
+    for (int i=0;i<n;i++) {\
+        CHECK(!fixed_bitset_get(&name.bitset, i), "Bit %d should be clear", i);\
+    }\
+    \
+    fixed_bitset_init(&name, type, n, 1);\
+    for (int i=0;i<n;i++) {\
+        CHECK(fixed_bitset_get(&name.bitset, i), "Bit %d should be set", i);\
+    }\
+    \
+    for (int i=n-1;i>=0;i--) {\
+        CHECK(fixed_bitset_get(&name.bitset, i), "Bit %d should be set", i);\
+        fixed_bitset_clear(&name.bitset, i);\
+        CHECK(!fixed_bitset_get(&name.bitset, i), "Bit %d should be clear", i);\
+        CHECK(!i == fixed_bitset_is_empty(&name.bitset), "Bitset should be empty once last bit is cleared");\
+    }\
+    \
+    fixed_bitset_init(&name, type, n, 1);\
+    for (int i=0;i<n;i++) {\
+        CHECK(fixed_bitset_get(&name.bitset, i), "Bit %d should be set", i);\
+    }\
+    for (int i=n-1;i>=0;i--) {\
+        CHECK(fixed_bitset_get(&name.bitset, i), "Bit %d should be set", i);\
+        fixed_bitset_clear(&name.bitset, i);\
+        CHECK(!fixed_bitset_get(&name.bitset, i), "Bit %d should be clear", i);\
+        CHECK(!i == fixed_bitset_is_empty(&name.bitset), "Bitset should be empty once last bit is cleared");\
+    }\
 
-    fixed_bitset_init(&b47, bitset47_t, 47, 1);
-    for (int i=0;i<47;i++) {
-        CHECK(fixed_bitset_get(&b47.bitset, i), "Bit %d should be set", i);
-    }
+    FIXED_BITSET_TESTS(b4, bitset4_t, 4);
+    FIXED_BITSET_TESTS(b47, bitset47_t, 47);
+    FIXED_BITSET_TESTS(b77, bitset77_t, 77);
 
-    for (int i=46;i>=0;i--) {
-        CHECK(fixed_bitset_get(&b47.bitset, i), "Bit %d should be set", i);
-        fixed_bitset_clear(&b47.bitset, i);
-        CHECK(!fixed_bitset_get(&b47.bitset, i), "Bit %d should be clear", i);
-        CHECK(!i == fixed_bitset_is_empty(&b47.bitset), "Bitset should be empty once last bit is cleared");
-    }
-
-    fixed_bitset_init(&b47, bitset47_t, 47, 1);
-    for (int i=0;i<47;i++) {
-        CHECK(fixed_bitset_get(&b47.bitset, i), "Bit %d should be set", i);
-    }
-    for (int i=0;i<47;i++) {
-        CHECK(fixed_bitset_get(&b47.bitset, i), "Bit %d should be set", i);
-        fixed_bitset_clear(&b47.bitset, i);
-        CHECK(!fixed_bitset_get(&b47.bitset, i), "Bit %d should be clear", i);
-        CHECK((i==46) == fixed_bitset_is_empty(&b47.bitset), "Bitset should be empty once last bit is cleared");
-    }
     puts("PASSED");
 }
