c5a4164c646c5c0b5421bc2f95b04ec2082e1b62
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2026-05-01 13:21:08 +0100

    Fix bazel build

--- test/kitchen_sink/kitchen_sink.c
+++ test/kitchen_sink/kitchen_sink.c
@@ -17,7 +17,7 @@
 #if LIB_PICO_BINARY_INFO
 #include "pico/binary_info.h"
 #endif
-#ifdef PICO_PSRAM_SIZE_BYTES
+#if !PICO_RP2040
 #include "hardware/flash.h"
 #include "hardware/psram.h"
 #include "hardware/xip_cache.h"
