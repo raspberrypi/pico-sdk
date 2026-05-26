9845591b54c6cca4425a309c9aa1ac2c86d27c1c
graham sanderson <graham.sanderson@raspberrypi.com>
2026-05-26 17:33:48 -0500

    Add docs and optional work around to rom_reset_usb_boot* for RP2350A2 QFN60 Arm to reboot into RISC-V when acitivity LED is requested Also renamed rp2040_rom_version rp2350_rom_version for consistency (it doesn't appear to be used)

--- src/rp2_common/pico_bootrom/bootrom.c
+++ src/rp2_common/pico_bootrom/bootrom.c
@@ -11,6 +11,8 @@
 #include "hardware/rcp.h"
 #endif
 
+// PICO_CONFIG: PICO_BOOTROM_WORKAROUND_A2_ACTIVITY_LED_BUG, Workaround RP2350A-A2 (QFN60) bug not displaying USB boot activity LED under Arm by making rom_reset_usb_boot functions reboot to RISC-V when the activity LED is specified, type=bool, default=0, group=pico_bootrom
+
 /// \tag::table_lookup[]
 
 void *rom_func_lookup(uint32_t code) {
@@ -31,6 +33,19 @@ bool rom_funcs_lookup(uint32_t *table, unsigned int count) {
     return ok;
 }
 
+// The activity LED on A2 QDN60 chips doesn't work in Arm mode, so boot into RISC-V if the user
+// really, really wants the activity LED
+#if PICO_RP2350_A2_SUPPORTED && PICO_RP2350A && !PICO_RISCV && PICO_BOOTROM_WORKAROUND_A2_ACTIVITY_LED_BUG
+#define rom_reboot_workaround(flags, delay_ms, p0, p1) ({ \
+    if (rp2350_rom_version() == 2 && ((p0) & BOOTSEL_FLAG_GPIO_PIN_SPECIFIED)) \
+        rom_reboot((flags) | REBOOT2_FLAG_REBOOT_TO_RISCV, delay_ms, p0, p1); \
+    else \
+        rom_reboot(flags, delay_ms, p0, p1); \
+})
+#else
+#define rom_reboot_workaround(flags, delay_ms, p0, p1) rom_reboot(flags, delay_ms, p0, p1)
+#endif
+
 
 void __attribute__((noreturn)) rom_reset_usb_boot(uint32_t usb_activity_gpio_pin_mask, uint32_t disable_interface_mask) {
 #ifdef ROM_FUNC_RESET_USB_BOOT
@@ -43,7 +58,7 @@ void __attribute__((noreturn)) rom_reset_usb_boot(uint32_t usb_activity_gpio_pin
         // the parameter is actually the gpio number, but we only care if BOOTSEL_FLAG_GPIO_PIN_SPECIFIED
         usb_activity_gpio_pin_mask = (uint32_t)__builtin_ctz(usb_activity_gpio_pin_mask);
     }
-    rom_reboot(REBOOT2_FLAG_REBOOT_TYPE_BOOTSEL | REBOOT2_FLAG_NO_RETURN_ON_SUCCESS, 10, flags, usb_activity_gpio_pin_mask);
+    rom_reboot_workaround(REBOOT2_FLAG_REBOOT_TYPE_BOOTSEL | REBOOT2_FLAG_NO_RETURN_ON_SUCCESS, 10, flags, usb_activity_gpio_pin_mask);
     __builtin_unreachable();
 #else
     panic_unsupported();
@@ -57,13 +72,14 @@ void __attribute__((noreturn)) rom_reset_usb_boot_extra(int usb_activity_gpio_pi
     func(usb_activity_gpio_pin < 0 ? 0 : (1u << usb_activity_gpio_pin), disable_interface_mask);
 #elif defined(ROM_FUNC_REBOOT)
     uint32_t flags = disable_interface_mask;
+    uint reboot2_flags = REBOOT2_FLAG_REBOOT_TYPE_BOOTSEL | REBOOT2_FLAG_NO_RETURN_ON_SUCCESS;
     if (usb_activity_gpio_pin >= 0) {
         flags |= BOOTSEL_FLAG_GPIO_PIN_SPECIFIED;
         if (usb_activity_gpio_pin_active_low) {
             flags |= BOOTSEL_FLAG_GPIO_PIN_ACTIVE_LOW;
         }
     }
-    rom_reboot(REBOOT2_FLAG_REBOOT_TYPE_BOOTSEL | REBOOT2_FLAG_NO_RETURN_ON_SUCCESS, 10, flags, (uint)usb_activity_gpio_pin);
+    rom_reboot_workaround(reboot2_flags, 10, flags, (uint)usb_activity_gpio_pin);
     __builtin_unreachable();
 #else
     panic_unsupported();
