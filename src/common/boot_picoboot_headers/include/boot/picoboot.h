fb62189129f16a1f57dbaf428b319542e2300a51
Graham Sanderson <graham.sanderson@raspberrypi.com>
2026-06-24 09:39:06 -0500

    Update src/common/boot_picoboot_headers/include/boot/picoboot.h

--- src/common/boot_picoboot_headers/include/boot/picoboot.h
+++ src/common/boot_picoboot_headers/include/boot/picoboot.h
@@ -167,7 +167,7 @@ struct __packed picoboot_otp_cmd {
  */
 struct __packed picoboot_get_info_cmd {
     uint8_t bType; ///< Info type selector
-    uint8_t bParam; ///< Primary parameter for the selected info type
+    uint8_t bParam; ///< Unused
     uint16_t wParam; ///< Secondary parameter for the selected info type
     uint32_t dParams[3]; ///< Additional parameters for the selected info type
 };
