db6585cfd7765fa56ddeb70b1fcc529dadfc2e66
will-v-pi <108662275+will-v-pi@users.noreply.github.com>
2026-06-30 15:57:43 +0100

    Update src/rp2_common/pico_low_power/low_power.c

--- src/rp2_common/pico_low_power/low_power.c
+++ src/rp2_common/pico_low_power/low_power.c
@@ -241,7 +241,7 @@ void low_power_set_pins_low_leakage64(uint64_t exclude_mask) {
     uint64_t include_mask = ~exclude_mask;
     gpio_set_dir_masked64(include_mask, 0);
     for (uint i=0; i < NUM_BANK0_GPIOS; i++) {
-        if (exclude_mask & (1u << i)) continue;
+        if (exclude_mask & (1ull << i)) continue;
         gpio_set_function(i, GPIO_FUNC_SIO);
         gpio_disable_pulls(i);
         gpio_set_input_enabled(i, false);
