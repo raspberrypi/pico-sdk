70849222b96db8a2baaed00567943f6b17c66868
graham sanderson <graham.sanderson@raspberrypi.com>
2026-07-14 18:43:55 -0500

    remove debugging printf

--- test/pio_encoding_test/pio_encoding_test.c
+++ test/pio_encoding_test/pio_encoding_test.c
@@ -22,7 +22,6 @@ int main() {
     PICOTEST_START_SECTION("check pio_encodings");
 
     PICOTEST_CHECK(0xa0e1 == encodings_program_instructions[encodings_offset_mov_osr_x], "mov osr, x");
-    printf("0x%x\n", pio_encode_mov(pio_osr, pio_x));
     PICOTEST_CHECK(0xa0e1 == pio_encode_mov(pio_osr, pio_x), "pio_encode_mov(pio_osr, pio_x)");
     PICOTEST_CHECK(0xa061 == encodings_program_instructions[encodings_offset_mov_pindirs_x], "mov pindirs, x");
     PICOTEST_CHECK(0xa061 == pio_encode_mov(pio_pindirs, pio_x), "pio_encode_mov(pio_pindirs, pio_x)");
