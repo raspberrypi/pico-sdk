53cb9c0e7dc8a1a7e9647b07cc789c210ef55850
will-v-pi <108662275+will-v-pi@users.noreply.github.com>
2026-09-26 15:30:17 +0100

    Add openssl and pkcs11 passwords/pins
    Add passing passwords/pins on the command line, and print warning when using the example signing method with non-kitchen_sink binaries

--- tools/example_keys/example_external_sign.cmake
+++ tools/example_keys/example_external_sign.cmake
@@ -1,22 +1,27 @@
 # Delete existing sig if present
 file(REMOVE ${SIG})
 
-if(1)
+if(DEFINED ENV{OPENSSL_PASS} AND DEFINED ENV{OPENSSL_KEY})
     # OpenSSL variant, for example with keys that require a password
-    # You must use the Make generator if you need to pass input to
-    # the shell (e.g. entering a password), as Ninja doesn't support that
+    # Set the OPENSSL_PASS environment variable to the password to unlock the key,
+    # and the OPENSSL_KEY environment variable to the path to the private key
 
-    # Copy across default public key
-    file(COPY_FILE ${CMAKE_CURRENT_LIST_DIR}/public.pem ${PUBKEY})
+    message(STATUS "Signing ${HASH} with OpenSSL")
+
+    # Get public key
+    execute_process(COMMAND
+        openssl ec -in $ENV{OPENSSL_KEY} -out ${PUBKEY} -pubout -passin env:OPENSSL_PASS
+    )
 
     # Use openssl to sign
     execute_process(COMMAND
-        openssl pkeyutl -in ${HASH} -inkey ${CMAKE_CURRENT_LIST_DIR}/private.pem -out ${SIG} -pkeyopt digest:sha256
+        openssl pkeyutl -in ${HASH} -inkey $ENV{OPENSSL_KEY} -out ${SIG} -pkeyopt digest:sha256 -passin env:OPENSSL_PASS
     )
-else()
+elseif(DEFINED ENV{PKCS11_PIN})
     # PKCS11 variant, for example with an HSM
-    # You must use the Make generator if you need to pass input to
-    # the shell (e.g. entering a pin), as Ninja doesn't support that
+    # Set the PKCS11_PIN environment variable to the pin to unlock the HSM
+
+    message(STATUS "Signing ${HASH} with pkcs11")
 
     # Get public key from HSM
     execute_process(COMMAND
@@ -30,6 +35,23 @@ else()
 
     # Use pkcs11-tool to sign
     execute_process(COMMAND
-        pkcs11-tool --sign --id 1 --mechanism ECDSA --input-file ${HASH} --output-file ${SIG} --signature-format openssl
+        pkcs11-tool --sign --id 1 --mechanism ECDSA --input-file ${HASH} --output-file ${SIG} --signature-format openssl --pin env:PKCS11_PIN
+    )
+else()
+    # Simple OpenSSL variant using example keys in SDK, which don't require a password
+
+    message(STATUS "Signing ${HASH} with OpenSSL, using SDK example keys")
+
+    # Copy across default public key
+    file(COPY_FILE ${CMAKE_CURRENT_LIST_DIR}/public.pem ${PUBKEY})
+
+    # Add any tests using this here - everything else gets a warning
+    if (NOT "${HASH}" MATCHES "kitchen_sink")
+        message(WARNING "Using example key files for external signing - do not use these for production")
+    endif()
+
+    # Use openssl to sign
+    execute_process(COMMAND
+        openssl pkeyutl -in ${HASH} -inkey ${CMAKE_CURRENT_LIST_DIR}/private.pem -out ${SIG} -pkeyopt digest:sha256
     )
 endif()
